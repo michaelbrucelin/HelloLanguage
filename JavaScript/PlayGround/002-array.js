b6bbe122499cc9b7232e3d5b818eeb88677876eb
micha <michaelbrucelin@gmail.com>
2021-03-23 20:59:49 -0400

    20210324-085949

--- JavaScript/PlayGround/002-array.js
+++ JavaScript/PlayGround/002-array.js
@@ -1,5 +1,7 @@
 //Array对象
-var arr = new Array();
+
+// 创建数组方式1
+var arr = new Array();  //var arr = new Array(3);  不强制要求指定数组的长度，更像C#的ArrayList
 arr[0] = "a";
 arr[1] = "b";
 arr[3] = "d";
@@ -9,7 +11,8 @@ console.log(arr.join());
 console.log(arr.join(';'));
 console.log(arr.toString());
 
-var arr2 = ["A", "B", "C", "D"];
+// 创建数组方式2
+var arr2 = Array("A", "B", "C", "D");
 for (let i = 0; i < arr2.length; i++) {
     console.log(i);
 }
@@ -20,6 +23,9 @@ for (let item in arr2) {
 for (let item in window) {
     console.log(item + ":\t" + window[item]);
 }
+
+// 创建数组方式3
+var arr3 = ["A", "B", "C", "D"];
 
 //Array对象的常用声明方法
 var arr0 = Array();
