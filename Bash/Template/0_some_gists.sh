0deb67f33f79f9a682ddf59835009e4fa827dc2a
micha <michaelbrucelin@gmail.com>
2021-05-03 03:03:32 +0000

    20210503-110332

--- Bash/Template/0_some_gists.sh
+++ Bash/Template/0_some_gists.sh
@@ -1,2 +1,2 @@
-# 记录localhost的公网ip
-echo `date -d '+12 hour' +'\%Y\%m\%d-\%H\%M\%S';curl ipinfo.io` >> /root/myselfpublicip.txt 2> /dev/null
+# 记录localhost的公网ip，使用0时区时间做记录
+echo `TZ=UTC date +'%Y%m%d-%H%M%S';curl ipinfo.io` >> /root/myselfpublicip.txt 2> /dev/null
