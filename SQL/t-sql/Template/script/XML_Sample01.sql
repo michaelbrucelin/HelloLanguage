7e8ecc550a46c1d3da0b929677f22c56ff3ee246
micha <michaelbrucelin@gmail.com>
2021-05-07 03:22:25 +0000

    20210507-112225

---
+++ SQL/t-sql/Template/script/XML_Sample01.sql
@@ -0,0 +1,22 @@
+-- xml遍历并更改节点的值
+
+declare @xml xml
+set @xml='<items>
+<item>AAA</item>
+<item>BBB</item>
+<item>CCC</item>
+</items>'
+select @xml
+
+declare @flag as int = 1
+
+while(@flag < 4)
+begin
+    declare @value varchar(10)
+    set @value = @flag
+    set @xml.modify('replace value of (/items/item[sql:variable("@flag")]/text())[1] with sql:variable("@value")')
+
+    set @flag = @flag + 1
+end
+
+select @xml
