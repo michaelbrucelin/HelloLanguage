9d83424214a6717c3619225032fc07f5db8c6053
mlin <491601759@qq.com>
2021-05-11 14:05:54 +0000

    20210511-220554

--- C#/PlayGround/Program.cs
+++ C#/PlayGround/Program.cs
@@ -7,6 +7,31 @@ namespace PlayGround
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            int[] arr = { 0, 1, 2, 3 };
+            func_arr(arr);
+            System.Console.WriteLine(arr[0]);
+
+            int i = 0, j = 0;
+            func_int(i);
+            func_int(ref j);
+            System.Console.WriteLine(i);
+            System.Console.WriteLine(j);
+        }
+
+        private static void func_arr(int[] arr)
+        {
+            arr[0] = 100;
+        }
+
+        private static void func_int(int i)
+        {
+            i = 100;
+        }
+
+        private static void func_int(ref int i)
+        {
+            i = 100;
         }
     }
 }
