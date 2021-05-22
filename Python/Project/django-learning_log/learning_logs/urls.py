4ef8f157bce83774f1bfe7346ee6291f8c089655
micha <michaelbrucelin@gmail.com>
2021-05-22 10:48:22 +0000

    20210522-184822

--- Python/Project/django-learning_log/learning_logs/urls.py
+++ Python/Project/django-learning_log/learning_logs/urls.py
@@ -12,4 +12,10 @@ urlpatterns = [
     path('topics/', views.topics, name='topics'),
     # 特定主题的详细页面
     path('topics/<int:topic_id>/', views.topic, name='topic'),
+    # 用于添加新主题的页面
+    path('new_topic/', views.new_topic, name='new_topic'),
+    # 用于添加新条目的页面
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+    # 用于编辑条目的页面
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
 ]
--- Python/Project/django-learning_log/learning_logs/views.py
+++ Python/Project/django-learning_log/learning_logs/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from .models import Topic
+from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
 
 # Create your views here.
 
@@ -23,3 +24,60 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')  # -表示降序排序
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+
+def new_topic(request):
+    """添加新主题"""
+    if request.method != 'POST':
+        # 未提交数据：创建一个新表单
+        form = TopicForm()
+    else:
+        # POST提交的数据，对数据进行处理
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+
+    # 显示空表单或指出表单数据无效
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
+
+
+def new_entry(request, topic_id):
+    """在特定主题中添加新条目"""
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        # 未提交数据：创建一个空表单
+        form = EntryForm()
+    else:
+        # POST提交的数据：对数据进行整理
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('learning_logs:topic', topic_id=topic_id)
+
+    # 显示空表单或指出表单数据无效
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
+
+
+def edit_entry(request, entry_id):
+    """编辑既有条目"""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        # 初次请求：使用当前条目填充表单
+        form = EntryForm(instance=entry)
+    else:
+        # POST提交的数据：对数据进行处理
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topic', topic_id=topic.id)
+
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'learning_logs/edit_entry.html', context)
