5808c01d1343d6e1a6d0e29ee8cd6a01934683f7
micha <michaelbrucelin@gmail.com>
2021-05-24 02:51:04 +0000

    20210524-105104

--- Python/Template/net-telnet.py
+++ Python/Template/net-telnet.py
@@ -9,15 +9,18 @@ password = getpass.getpass()
 
 tn = telnetlib.Telnet(HOST)
 
-tn.read_until(b"Username: ")
+tn.read_until(b"Username:")
 tn.write(user.encode('ascii') + b"\n")
 if password:
-    tn.read_until(b"Password: ")
+    tn.read_until(b"Password:")
     tn.write(password.encode('ascii') + b"\n")
 
-# tn.write(b"ls\n")
+# tn.write(b"dir\n")  # or ls
 # tn.write(b"exit\n")
 tn.write(b"display local-user\n")
-tn.write(b"exit\n")
+# display local-user命令没有一次性输出所有用户，而是自动应用了类似于bash的 | less的分页功能
+# 所以这里发送了10个空格（华为的文档中有screen-length与screen-width两个设置命令，测试无效）
+tn.write(b" "*10)
+tn.write(b"quit\n")   # or exit
 
 print(tn.read_all().decode('ascii'))
