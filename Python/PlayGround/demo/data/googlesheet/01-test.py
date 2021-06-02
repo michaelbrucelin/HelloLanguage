0f76c2ce6429dc223f235bc61e2d7e553e05aa6f
michael brucelin <michaelbrucelin@gmail.com>
2021-06-02 10:37:36 +0800

    20210602-1037

--- Python/PlayGround/demo/data/googlesheet/01-test.py
+++ Python/PlayGround/demo/data/googlesheet/01-test.py
@@ -1,2 +1,8 @@
 # google sheet自己的api对用户并不友好，这里使用第三方模块ezsheets
 # pip install ezsheets
+
+"""
+google的说明文档：https://developers.google.com/sheets/api/quickstart/python
+需要先启用Google Sheets API与Google Drive API：https://console.cloud.google.com/apis/dashboard
+然后创建凭据，下载，重命名为credentials-sheets.json并放在脚本相同的目录下
+"""
