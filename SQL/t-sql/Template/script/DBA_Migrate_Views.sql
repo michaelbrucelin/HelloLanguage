f47620073f5f4c204b32c6c75666f0d686fbdb6b
micha <michaelbrucelin@gmail.com>
2021-06-30 10:01:57 +0000

    20210630-180157

--- SQL/t-sql/Template/script/DBA_Migrate_Views.sql
+++ SQL/t-sql/Template/script/DBA_Migrate_Views.sql
@@ -4,6 +4,7 @@ declare c cursor fast_forward for
 select TABLE_NAME, TRIM(VIEW_DEFINITION) as VIEW_DEFINITION
 from [RemoteServer].[RemoteDatabase].[INFORMATION_SCHEMA].[VIEWS]
 where TABLE_NAME like 'vw%' and VIEW_DEFINITION not like '%formtable[_]main[_][0-9]%'
+order by TABLE_NAME
 
 open c
 fetch next from c into @viewname, @definition
