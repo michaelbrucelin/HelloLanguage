6e05817995fe36c096112834d35801b4e513b4a1
micha <michaelbrucelin@gmail.com>
2021-07-29 09:25:19 +0000

    20210729-172519

--- Batchfile/PowerShell/Template/0_some_gists.ps1
+++ Batchfile/PowerShell/Template/0_some_gists.ps1
@@ -1,3 +1,10 @@
 # 脚本中从终端获取密码，并使用密码（Linux版powershell测试无效）
 $pwdsecure = Read-Host "Enter a Password" -AsSecureString
 [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($pwdsecure))
+
+# 清空powershell的history
+function MyClear-History() {
+    Clear-History
+    [Microsoft.PowerShell.PSConsoleReadLine]::ClearHistory()
+    Clear-Content $(Get-PSReadlineOption).HistorySavePath
+}
