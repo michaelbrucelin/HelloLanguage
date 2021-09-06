40aebe060f3d2eb6e738de3a442100fbec3a0e85
micha <michaelbrucelin@gmail.com>
2021-09-06 01:44:38 +0000

    20210906-094438

---
+++ Go/PlayGround/01-hellogolang.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
+
+/* 运行方法
+1. 当做脚本来运行
+# go run 01-hellogolang.go
+Hello, World!
+
+2. 编译运行
+# go build 01-hellogolang.go
+# ls
+01-hellogolang  01-hellogolang.go
+# ./01-hellogolang
+Hello, World!
+*/
