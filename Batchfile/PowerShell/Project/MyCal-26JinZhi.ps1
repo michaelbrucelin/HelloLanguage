f68233e0b80e01d7665af00fb2e82a20c51f8361
michaelbrucelin <michaelbrucelin@gmail.com>
2021-10-28 02:01:44 -0700

    20211028-170144

---
+++ Batchfile/PowerShell/Project/MyCal-26JinZhi.ps1
@@ -0,0 +1,56 @@
+﻿function MyCal-26JinZhi() {
+    param(
+        [string]$inputDigit,
+        [ValidateSet('DecTo26','26ToDec')]
+        [string]$type
+    )
+
+    if($type -eq "DecTo26") {
+
+        [int[]] $result = @()    #$result = ""
+        $temp = [System.Convert]::ToInt32($inputDigit)
+
+        do {
+            if(($temp%26) -eq 0) {
+                $result += 26    #$result = "26," + $result
+                $temp = [System.Math]::Floor($temp/26) - 1
+            }
+            else {
+                $result += $temp%26    #$result = ($temp%26).ToString() + "," + $result
+                $temp = [System.Math]::Floor($temp/26)
+            }
+        }
+        while($temp -gt 26)
+
+        if($temp -ne 0) {
+            #$result = $temp.ToString() + "," + $result
+            $result += $temp
+        }
+
+        # 反转数组
+        $result = $result[($result.Count-1)..0]
+        #$result = $result.Trim(",")
+
+        for($i=0; $i -lt $result.Count; $i++){
+            #转成ASCII
+            $result[$i] = $result[$i]+64
+        }
+
+        Write-Output ([char[]]$result -join '')
+    }
+    else {
+        [int] $result = 0
+
+        $temp = $inputDigit.ToCharArray()
+        $temp = [int[]]$temp
+        for($i=0; $i -lt $temp.Count; $i++){
+            #转成ASCII
+            $temp[$i] = $temp[$i]-64
+        }
+        
+        for($i=0; $i -lt $temp.Count; $i++){
+            $result = $result + $temp[$i]*[System.Math]::Pow(26,$temp.Count-1-$i)
+        }
+        Write-Output $result
+    }
+}
---
+++ Batchfile/PowerShell/Project/MyGet-RandomPassword.ps1
@@ -0,0 +1,51 @@
+﻿function MyGet-RandomPassword() {
+    param(
+        [int]$count = 1,
+        [int]$length = 36,
+        [ValidateSet('digit', 'letter', 'LETTER', 'digit-letter', 'digit-LETTER', 'letter-LETTER', 'd-l-L', 'ascii', 'customize')]
+        [string]$metaType = 'ascii',
+        [char[]]$metaChar = [char[]]('')
+    )
+ 
+    if ($metaType -ceq "digit") {
+        $metaChar = [char[]](48..57)
+    }
+    elseif ($metaType -ceq "letter") {
+        $metaChar = [char[]](97..122)
+    }
+    elseif ($metaType -ceq "LETTER") {
+        $metaChar = [char[]](65..90)
+    }
+    elseif ($metaType -ceq "digit-letter") {
+        $metaChar = [char[]](48..57 + 97..122)
+    }
+    elseif ($metaType -ceq "digit-LETTER") {
+        $metaChar = [char[]](48..57 + 65..90)
+    }
+    elseif ($metaType -ceq "letter-LETTER") {
+        $metaChar = [char[]](65..90 + 97..122)
+    }
+    elseif ($metaType -ceq "d-l-L") {
+        $metaChar = [char[]](48..57 + 65..90 + 97..122)
+    }
+    elseif ($metaType -ceq "ascii") {
+        $metaChar = [char[]](48..57 + 65..90 + 97..122 + 35..37 + 43 + 45 + 60..64)
+    }
+    elseif ($metaType -ceq "customize") {
+        if ($metaChar.Length -eq 0) {
+            Write-Host -ForegroundColor Red '自定义时必须使用$metaChar参数指定可选的字符';
+            return;
+        }
+    }
+
+    $result = @()
+    for ($i = 1; $i -le $count; $i++) {
+        $item = ""
+        for ($j = 1; $j -le $length; $j++) {
+            $item += ($metaChar | Get-Random | % { [char]$_ })
+        }
+        $result += $item
+    }
+
+    return $result
+}
