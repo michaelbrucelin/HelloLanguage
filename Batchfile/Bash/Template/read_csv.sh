87d0a0b68a169364e98c69d7e33da869058e225a
michaelbrucelin <michaelbrucelin@gmail.com>
2021-11-30 11:41:29 +0800

    20211130-114129

--- Batchfile/Bash/Template/read_csv.sh
+++ Batchfile/Bash/Template/read_csv.sh
@@ -9,3 +9,16 @@ done < <(cat files/kvinfo.csv)
 while IFS=$',' read key value; do
     echo key:$key value:$value
 done < <(cat files/kvinfo.csv)
+
+# 第三种方式，读取进字典
+declare -A dic
+while IFS=$',' read key value; do
+    dic[$key]=$value
+done < <(cat files/kvinfo.csv)
+
+# 验证单个值
+echo ${dic[key01]}
+# 遍历整个字典
+for key in ${!dic[@]}; do
+    echo ${key} ${dic[${key}]}
+done
