fddd769e0d615004bff6595369e98098d1323013
michaelbrucelin <michaelbrucelin@gmail.com>
2021-12-20 09:04:40 +0800

    20211220-090440

---
+++ C&C++/C/Book/C语言程序设计/第02章 C语言基本概念/01-pun.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("To C, or not to C: that is the question.\n");
+    printf("Brevity is the soul of wit.\n --Shakespeare\n");
+
+    return 0;
+}
---
+++ C&C++/C/Book/C语言程序设计/第02章 C语言基本概念/01-pun2.c
@@ -0,0 +1,35 @@
+/***********************************************************
+ * Name: pun.c                                             *
+ * Purpose: Prints a bad pun.                              *
+ * Author: K. N. King                                      *
+ **********************************************************/
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("To C, or not to C: that is the question.\n");
+
+    return 0;
+}
+
+//常用的注释形式
+
+/* Name: pun.c */
+/* Purpose: Prints a bad pun. */
+/* Author: K. N. King */
+
+/* Name: pun.c
+Purpose: Prints a bad pun.
+Author: K. N. King */
+
+/* Name: pun.c
+Purpose: Prints a bad pun.
+Author: K. N. King
+*/
+
+/*
+ * Name: pun.c
+ * Purpose: Prints a bad pun.
+ * Author: K. N. King
+ */
