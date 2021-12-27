d9e0d6df26b0f7a8e2138e78d2dab7e3220af298
michaelbrucelin <michaelbrucelin@gmail.com>
2021-12-27 09:13:24 +0800

    20211227-091324

---
+++ C&C++/C/Book/C语言程序设计/第07章 基本类型/02-length.c
@@ -0,0 +1,19 @@
+/* Determines the length of a message */
+#include <stdio.h>
+
+int main(void)
+{
+    char ch;
+    int len = 0;
+
+    printf("Enter a message: ");
+    ch = getchar();
+    while (ch != '\n')
+    {
+        len++;
+        ch = getchar();
+    }
+    printf("Your message was %d character(s) long.\n", len);
+
+    return 0;
+}
---
+++ C&C++/C/Book/C语言程序设计/第07章 基本类型/02-length2.c
@@ -0,0 +1,14 @@
+/* Determines the length of a message */
+#include <stdio.h>
+
+int main(void)
+{
+    int len = 0;
+
+    printf("Enter a message: ");
+    while (getchar() != '\n')
+        len++;
+    printf("Your message was %d character(s) long.\n", len);
+
+    return 0;
+}
