26db08bf08b963ee8211b293bd3f0041049ee432
michaelbrucelin <michaelbrucelin@gmail.com>
2022-01-05 10:05:48 +0800

    20220105-100548

--- Language/C&C++/C/Book/C语言程序设计/第12章 指针和数组/01-reverse3.c
+++ Language/C&C++/C/Book/C语言程序设计/第12章 指针和数组/01-reverse3.c
@@ -6,9 +6,11 @@
 int main(void)
 {
     int a[N], *p;
+
     printf("Enter %d numbers: ", N);
     for (p = a; p < a + N; p++)
         scanf("%d", p);
+
     printf("In reverse order:");
     for (p = a + N - 1; p >= a; p--)
         printf(" %d", *p);
