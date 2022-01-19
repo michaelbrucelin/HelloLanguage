c05d743b8345733b2d28fab3e4f1da52ba29e433
michaelbrucelin <michaelbrucelin@gmail.com>
2022-01-19 10:03:33 +0800

    20220119-100333

---
+++ Language/C&C++/C/Book/C语言程序设计/第19章 程序设计/stackADT.h
@@ -0,0 +1,12 @@
+#ifndef STACKADT_H
+#define STACKADT_H
+#include <stdbool.h> /* C99 only */
+typedef struct stack_type *Stack;
+Stack create(void);
+void destroy(Stack s);
+void make_empty(Stack s);
+bool is_empty(Stack s);
+bool is_full(Stack s);
+void push(Stack s, int i);
+int pop(Stack s);
+#endif
