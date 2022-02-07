0fc888893fbb9a0378d15a98b06f076595e00334
michael brucelin <michaelbrucelin@gmail.com>
2022-02-07 14:55:25 +0800

    20220207-1455

---
+++ Language/Java/PlayGround/TestHttp.java
@@ -0,0 +1,15 @@
+import java.net.*;
+import java.io.*;
+
+public class URLConnectionReader {
+    public static void main(String[] args) throws Exception {
+        URL oracle = new URL("http://www.oracle.com/");
+        URLConnection yc = oracle.openConnection();
+        BufferedReader in = new BufferedReader(new InputStreamReader(
+                                    yc.getInputStream()));
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) 
+            System.out.println(inputLine);
+        in.close();
+    }
+}
