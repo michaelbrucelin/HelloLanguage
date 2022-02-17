45ccfc4a38015279bd524ce2d5ad3313d8d67319
mlin <36387756+michaelbrucelin@users.noreply.github.com>
2022-02-17 22:07:42 +0800

    20220217-2207

--- Language/Java/PlayGround/IntelliJ IDEA/ITHEIMA/javase/hello-app/src/com/itheima/hello/HelloJava.java
+++ Language/Java/PlayGround/IntelliJ IDEA/ITHEIMA/javase/hello-app/src/com/itheima/hello/HelloJava.java
@@ -1,7 +1,7 @@
 package com.itheima.hello;
 
-public class HelloWorld {
+public class HelloJava {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello, Java!");
     }
 }
