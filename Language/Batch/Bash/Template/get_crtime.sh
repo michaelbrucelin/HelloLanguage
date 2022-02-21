01001cf21321658699e238ea344cf367e885770f
michaelbrucelin <michaelbrucelin@gmail.com>
2022-02-21 10:22:46 +0800

    20220221-102246

--- Language/Batch/Bash/Template/get_crtime.sh
+++ Language/Batch/Bash/Template/get_crtime.sh
@@ -4,7 +4,7 @@ get_crtime() {
         inode=$(stat -c '%i' "${target}")
         fs=$(df --output=source "${target}" | tail -1)
         crtime=$(debugfs -R 'stat <'"${inode}"'>' "${fs}" 2>/dev/null | grep -oP 'crtime.*--\s*\K.*')
-        
+
         printf "%s\t%s\n" "${target}" "${crtime}"
     done
 }
