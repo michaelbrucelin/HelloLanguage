37872b55566c069d6899ce6293f0758d92730c04
michaelbrucelin <michaelbrucelin@gmail.com>
2022-03-01 09:51:28 +0800

    20220301-095128

---
+++ Language/Java/PlayGround/HelloWorld2.java
@@ -0,0 +1,12 @@
+package Java.PlayGround;
+
+public class HelloWorld2 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+// # javac Java/PlayGround/HelloWorld2.java # 编译不要求完整的目录，可以直接在当前目录执行 javac
+// HelloWorld2.java
+// # java Java/PlayGround/HelloWorld2       # 需要在指定的目录下执行，必须切换到包的父目录（这里是Language目录）下才能执行
+// Hello World!
---
+++ Language/Java/PlayGround/ValueReference.java
@@ -0,0 +1,53 @@
+/**
+ * Java中引用类型的参数，也是值引用，即复制了对象的一个新的引用，也就是说，方法无法更改变量的引用。
+ * 下面示例可以看出，在方法内部，确实交换了变量的引用，但是在方法外部，并没有真的交换了变量的引用。
+ * 运行结果：
+ * Before: a=Alice
+ * Before: b=Bob
+ * Begin of method: x=Alice
+ * Begin of method: y=Bob
+ * End of method: x=Bob
+ * End of method: y=Alice
+ * After: a=Alice
+ * After: b=Bob
+ */
+public class ValueReference {
+    public static void main(String[] args) {
+        Employee a = new Employee("Alice", 70000);
+        Employee b = new Employee("Bob", 60000);
+        System.out.println("Before: a=" + a.getName());
+        System.out.println("Before: b=" + b.getName());
+
+        swap(a, b);
+        System.out.println("After: a=" + a.getName());
+        System.out.println("After: b=" + b.getName());
+    }
+
+    public static void swap(Employee x, Employee y) {
+        System.out.println("Begin of method: x=" + x.getName());
+        System.out.println("Begin of method: y=" + y.getName());
+        Employee temp = x;
+        x = y;
+        y = temp;
+        System.out.println("End of method: x=" + x.getName());
+        System.out.println("End of method: y=" + y.getName());
+    }
+}
+
+class Employee {
+    private String name;
+    private double salary;
+
+    public Employee(String n, double s) {
+        name = n;
+        salary = s;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+}
