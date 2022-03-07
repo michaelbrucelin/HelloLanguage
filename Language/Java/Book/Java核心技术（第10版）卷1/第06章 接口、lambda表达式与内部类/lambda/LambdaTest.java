b3a2fbab187ffd07478ccc2e4009f6f6cfe8d6d0
michaelbrucelin <michaelbrucelin@gmail.com>
2022-03-07 09:03:17 +0800

    20220307-090317

--- Language/Java/Book/Java核心技术（第10版）卷1/第06章 接口、lambda表达式与内部类/lambda/LambdaTest.java
+++ Language/Java/Book/Java核心技术（第10版）卷1/第06章 接口、lambda表达式与内部类/lambda/LambdaTest.java
@@ -10,8 +10,7 @@ import javax.swing.Timer;
  */
 public class LambdaTest {
     public static void main(String[] args) {
-        String[] planets = new String[] { "Mercury", "Venus", "Earth", "Mars",
-                "Jupiter", "Saturn", "Uranus", "Neptune" };
+        String[] planets = new String[] { "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune" };
         System.out.println(Arrays.toString(planets));
         System.out.println("Sorted in dictionary order:");
         Arrays.sort(planets);
