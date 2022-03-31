56bc00ea83208db38b00a7b581498a13fefc9d73
michaelbrucelin <michaelbrucelin@gmail.com>
2022-03-31 11:25:30 +0800

    20220331-112530

--- Language/C#/PlayGround/HelloWorld.cs
+++ Language/C#/PlayGround/HelloWorld.cs
@@ -1,16 +1,27 @@
 using System;
 
-public class HelloWorld
+namespace HelloWorld
 {
-    public static void Main()
+    public class HelloWorld
     {
-        Console.WriteLine("Hello, World!");
+        public static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
     }
 }
 
 /*
 没有找到更优雅的方式直接运行 .cs 代码，这里使用powershell的反射机制运行。
 (Add-Type -Path "HelloWorld.cs" -PassThru)::Main()
+
 将上面的ps脚本封装为方法，方便日后调用：
-MyRun-Dotnet ./HelloWorld.cs
+function myrun-csharp() {
+    pwsh -Command "(Add-Type -Path '${1}' -PassThru)::Main()"
+}
+myrun-charp HelloWorld.cs
+
+而且这样使用还有很多的限制，目前发现：
+1. 只能含有一个类；
+2. 只能包含静态方法；
 */
--- Language/C#/Template/Http/HttpClientGet.cs
+++ Language/C#/Template/Http/HttpClientGet.cs
@@ -3,40 +3,43 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
-// TODO MyRun-Dotnet ./HttpClientGet.cs 报错
+// TODO 未完成
 
-public class HttpClientGet
+namespace SynthesisTool
 {
-    public static void Main()
+    public class HttpClientGet
     {
-        Demo1();
-        Console.WriteLine("=".MyRepeat(64));
-    }
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            Demo1();
+            Console.WriteLine(StringRepeat("=", 64));
+        }
 
-    private static void Demo1()
-    {
-        var client = new HttpClient();
-        var result = client.GetAsync("http://www.microsoft.com").Result;
-        var content = result.Content.ReadAsStringAsync().Result;
-        Console.WriteLine(content);
-    }
+        private static void Demo1()
+        {
+            var client = new HttpClient();
+            var result = client.GetAsync("http://www.microsoft.com").Result;
+            var content = result.Content.ReadAsStringAsync().Result;
+            Console.WriteLine(content);
+        }
 
-    private static readonly HttpClient client = new HttpClient();
+        private static readonly HttpClient client = new HttpClient();
 
-    private static async Task<string> GetStringAsync(string url)
-    {
-        // var responseString = await client.GetStringAsync("http://www.example.com/recepticle.aspx");
-        var response = await client.GetAsync(url);
-        return await response.Content.ReadAsStringAsync();
-    }
-}
+        /*
+        private static async Task<string> GetStringAsync(string url)
+        {
+            // var responseString = await client.GetStringAsync("http://www.example.com/recepticle.aspx");
+            var response = await client.GetAsync(url);
+            return await response.Content.ReadAsStringAsync();
+        }
+        */
 
-public static class StringExtensions
-{
-    public static string MyRepeat(this string s, int n)
-    {
-        return new StringBuilder(s.Length * n)
-                    .AppendJoin(s, new string[n + 1])
-                    .ToString();
+        private static string StringRepeat(string s, int n)
+        {
+            return new StringBuilder(s.Length * n)
+                        .AppendJoin(s, new string[n + 1])
+                        .ToString();
+        }
     }
 }
