8479b48a960e7a4dcade5c31b9f546103993bc1c
michaelbrucelin <michaelbrucelin@gmail.com>
2022-04-01 17:25:34 +0800

    20220401-172533

---
+++ Language/C#/Book/LINQ实战/第01章 LINQ简介/HelloLinq/HelloLinq.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+static class HelloWorld
+{
+    static void Main()
+    {
+        string[] words = { "hello", "wonderful", "linq", "beautiful", "world" };
+
+        // Get only short words
+        var shortWords = from word in words
+                         where word.Length <= 5
+                         select word;
+
+        // Print each word out
+        foreach (var word in shortWords)
+            Console.WriteLine(word);
+    }
+}
