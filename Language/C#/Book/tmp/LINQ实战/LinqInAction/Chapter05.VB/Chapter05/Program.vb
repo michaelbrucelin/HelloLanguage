d1cf778ad9865400a6917e83dad87c16753566b8
michaelbrucelin <michaelbrucelin@gmail.com>
2022-04-01 17:19:23 +0800

    20220401-171923

--- Language/C#/Book/tmp/LINQ实战/LinqInAction/Chapter05.VB/Chapter05/Program.vb
+++
@@ -1,9 +0,0 @@
-﻿Module Program
-
-  Sub Main()
-    AppDomain.CurrentDomain.SetData("DataDirectory", System.IO.Path.GetFullPath("..\..\..\..\Data\"))
-    Application.EnableVisualStyles()
-    Application.Run(New FormMain())
-  End Sub
-
-End Module
--- Language/C#/Book/tmp/LINQ实战/LinqInAction/Chapter06to08.VB/Common/SampleClasses/AuditTracking.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class AuditTracking
-    Public ID As Integer
-    Public TableName As String
-    Public UserName As String
-    Public AccessDate As DateTime
-End Class
