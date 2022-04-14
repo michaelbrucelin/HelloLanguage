1337d3efa4d4585619eab3aeb261b6a242131742
michaelbrucelin <michaelbrucelin@gmail.com>
2022-04-14 13:01:52 +0800

    20220414-130152

--- Language/SQL/t-sql/Template/script/Syntax_DynamicSql.sql
+++ Language/SQL/t-sql/Template/script/Syntax_DynamicSql.sql
@@ -3,9 +3,9 @@
 /*
 1、exec(@sql)            --exec()支持正则和Unicode字符串作为输入
 2、exec sp_executesql    --sp_executesql只支持Unicode字符串作为输入
-    @stmt = @sql,
-    @params = N'@paraInSql as DATATYPE [OUTPUT]',  
-    @paraInSql = @paraExeValue [OUTPUT]
+            @stmt = @sql,
+            @params = N'@paraInSql as DATATYPE [OUTPUT]',
+            @paraInSql = @paraExeValue [OUTPUT]
 
 --@stmt = @sql,             
 --类似于存储过程的主体，可以引用输入和输出参数。这部分通常动态构造。
@@ -26,7 +26,7 @@ set @sql = N'if(exists(select 1 from dbo.country_'+@countryCode+
               begin set @resultInsideOut = 1 end else begin set @resultOut = 0 end'
 
 exec sp_executesql @stmt = @sql,
-   @params = N'@cnt as int, @resultInsideOut as int output',
-   @cnt = 1, @resultInsideOut = @resultOutside output
+                   @params = N'@cnt as int, @resultInsideOut as int output',
+                   @cnt = 1, @resultInsideOut = @resultOutside output
 
 select @result
