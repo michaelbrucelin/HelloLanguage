cda393133564cc56c8080fea207279497b511d69
michaelbrucelin <michaelbrucelin@gmail.com>
2022-04-16 15:57:33 +0800

    20220416-155733

---
+++ Language/Go/Book/Go程序设计语言/第01章 入门/helloworld.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, world!")
+}
+
+/*
+# 一次性测试代码
+# go run helloworld.go
+Hello, world!
+
+# 编译执行
+# go build helloworld.go
+# ll
+total 1720
+-rwxr-xr-x 1 root root 1754329 Apr 16 07:53 helloworld
+-rw-r--r-- 1 root root     158 Apr 16 07:53 helloworld.go
+
+# ./helloworld
+Hello, world!
+*/
