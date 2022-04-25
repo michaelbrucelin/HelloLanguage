27506edc79f0877a0854b494a52006c5b51d64f0
michaelbrucelin <michaelbrucelin@gmail.com>
2022-04-25 17:54:33 +0800

    20220425-175433

---
+++ Language/Java/PlayGround/DateTime.java
@@ -0,0 +1,32 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateTime {
+    public static void main(String[] args) {
+        LocalDate myDate = LocalDate.now();
+        System.out.println(myDate);
+
+        LocalTime myTime = LocalTime.now();
+        System.out.println(myTime);
+
+        LocalDateTime myDatetime = LocalDateTime.now();
+        System.out.println(myDatetime);
+
+        System.out.println("Before formatting: " + myDatetime);
+        DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        String formattedDate = myDatetime.format(myFormatObj);
+        System.out.println("After formatting: " + formattedDate);
+    }
+}
+
+/*
+javac DateTime.java
+java DateTime
+> 2022-04-25
+> 09:53:43.105760
+> 2022-04-25T09:53:43.106070
+> Before formatting: 2022-04-25T09:53:43.106070
+> After formatting: 25-04-2022 09:53:43
+*/
