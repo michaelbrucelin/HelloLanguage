8419ec849ff13e54addba1f22a92d98e72968d11
michaelbrucelin <michaelbrucelin@gmail.com>
2022-05-19 07:58:25 +0800

    20220519-075825

---
+++ Language/Ruby/Book/Ruby编程语言/第01章 导言/oo.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/ruby
+
+# ruby是面向对象的
+puts 1.class
+puts 0.0.class
+puts "Ruby".class
+puts true.class
+puts false.class
+puts nil.class
+
+=begin
+ruby oo.rb
+> Integer
+> Float
+> String
+> TrueClass
+> FalseClass
+> NilClass
+=end
