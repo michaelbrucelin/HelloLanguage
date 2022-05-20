699970bb1e10894a3f514eecdd5e12db384fedfa
michaelbrucelin <michaelbrucelin@gmail.com>
2022-05-20 08:59:00 +0800

    20220520-085900

---
+++ Language/Ruby/Book/Ruby编程语言/第02章 Ruby程序的结构和运行/comment.rb
@@ -0,0 +1,34 @@
+#
+# Rdoc comments use a simple markup grammar like those used in wikis.
+#
+# Separate paragraphs with a blank line.
+#
+# = Headings
+#
+# Headings begin with an equals sign
+#
+# == Sub-Headings
+# The line above produces a subheading.
+# === Sub-Sub-Heading
+# And so on.
+#
+# = Examples
+#
+# Indented lines are displayed verbatim in code font.
+# Be careful not to indent your headings and lists, though.
+#
+# = Lists and Fonts
+#
+# List items begin with * or -. Indicate fonts with punctuation or HTML:
+# * _italic_ or <i>multi-word italic</i>
+# * *bold* or <b>multi-word bold</b>
+# * +code+ or <tt>multi-word code</tt>
+#
+# 1. Numbered lists begin with numbers.
+# 99. Any number will do; they don't have to be sequential.
+# 1. There is no way to do nested lists.
+#
+# The terms of a description list are bracketed:
+# [item 1] This is a description of item 1
+# [item 2] This is a description of item 2
+#
