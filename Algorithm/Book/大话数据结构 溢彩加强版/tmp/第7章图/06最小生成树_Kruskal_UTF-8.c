80a5f9df1d950d0712b34cf074297fd76f77dec9
michaelbrucelin <michaelbrucelin@gmail.com>
2022-05-23 09:08:46 +0800

    20220523-090846

--- Algorithm/Book/大话数据结构 溢彩加强版/tmp/第7章图/06最小生成树_Kruskal_UTF-8.c
+++ Algorithm/Book/大话数据结构 溢彩加强版/tmp/第7章图/06最小生成树_Kruskal_UTF-8.c
@@ -1,7 +1,7 @@
-#include "stdio.h"    
-#include "stdlib.h"   
+#include "stdio.h"
+#include "stdlib.h"
 
-#include "math.h"  
+#include "math.h"
 #include "time.h"
 
 #define OK 1
@@ -9,7 +9,7 @@
 #define TRUE 1
 #define FALSE 0
 
-typedef int Status;	/* Status是函数的类型,其值是函数结果状态代码，如OK等 */
+typedef int Status; /* Status是函数的类型,其值是函数结果状态代码，如OK等 */
 
 #define MAXEDGE 20
 #define MAXVEX 20
@@ -17,158 +17,155 @@ typedef int Status;	/* Status是函数的类型,其值是函数结果状态代�
 
 typedef struct
 {
-	int arc[MAXVEX][MAXVEX];
-	int numVertexes, numEdges;
-}MGraph;
+    int arc[MAXVEX][MAXVEX];
+    int numVertexes, numEdges;
+} MGraph;
 
 typedef struct
 {
-	int begin;
-	int end;
-	int weight;
-}Edge;   /* 对边集数组Edge结构的定义 */
+    int begin;
+    int end;
+    int weight;
+} Edge; /* 对边集数组Edge结构的定义 */
 
 /* 构件图 */
 void CreateMGraph(MGraph *G)
 {
-	int i, j;
+    int i, j;
 
-	/* printf("请输入边数和顶点数:"); */
-	G->numEdges=15;
-	G->numVertexes=9;
+    /* printf("请输入边数和顶点数:"); */
+    G->numEdges = 15;
+    G->numVertexes = 9;
 
-	for (i = 0; i < G->numVertexes; i++)/* 初始化图 */
-	{
-		for ( j = 0; j < G->numVertexes; j++)
-		{
-			if (i==j)
-				G->arc[i][j]=0;
-			else
-				G->arc[i][j] = G->arc[j][i] = GRAPH_INFINITY;
-		}
-	}
+    for (i = 0; i < G->numVertexes; i++) /* 初始化图 */
+    {
+        for (j = 0; j < G->numVertexes; j++)
+        {
+            if (i == j)
+                G->arc[i][j] = 0;
+            else
+                G->arc[i][j] = G->arc[j][i] = GRAPH_INFINITY;
+        }
+    }
 
-	G->arc[0][1]=10;
-	G->arc[0][5]=11; 
-	G->arc[1][2]=18; 
-	G->arc[1][8]=12; 
-	G->arc[1][6]=16; 
-	G->arc[2][8]=8; 
-	G->arc[2][3]=22; 
-	G->arc[3][8]=21; 
-	G->arc[3][6]=24; 
-	G->arc[3][7]=16;
-	G->arc[3][4]=20;
-	G->arc[4][7]=7; 
-	G->arc[4][5]=26; 
-	G->arc[5][6]=17; 
-	G->arc[6][7]=19; 
+    G->arc[0][1] = 10;
+    G->arc[0][5] = 11;
+    G->arc[1][2] = 18;
+    G->arc[1][8] = 12;
+    G->arc[1][6] = 16;
+    G->arc[2][8] = 8;
+    G->arc[2][3] = 22;
+    G->arc[3][8] = 21;
+    G->arc[3][6] = 24;
+    G->arc[3][7] = 16;
+    G->arc[3][4] = 20;
+    G->arc[4][7] = 7;
+    G->arc[4][5] = 26;
+    G->arc[5][6] = 17;
+    G->arc[6][7] = 19;
 
-	for(i = 0; i < G->numVertexes; i++)
-	{
-		for(j = i; j < G->numVertexes; j++)
-		{
-			G->arc[j][i] =G->arc[i][j];
-		}
-	}
-
+    for (i = 0; i < G->numVertexes; i++)
+    {
+        for (j = i; j < G->numVertexes; j++)
+        {
+            G->arc[j][i] = G->arc[i][j];
+        }
+    }
 }
 
 /* 交换权值 以及头和尾 */
-void Swapn(Edge *edges,int i, int j)
+void Swapn(Edge *edges, int i, int j)
 {
-	int temp;
-	temp = edges[i].begin;
-	edges[i].begin = edges[j].begin;
-	edges[j].begin = temp;
-	temp = edges[i].end;
-	edges[i].end = edges[j].end;
-	edges[j].end = temp;
-	temp = edges[i].weight;
-	edges[i].weight = edges[j].weight;
-	edges[j].weight = temp;
+    int temp;
+    temp = edges[i].begin;
+    edges[i].begin = edges[j].begin;
+    edges[j].begin = temp;
+    temp = edges[i].end;
+    edges[i].end = edges[j].end;
+    edges[j].end = temp;
+    temp = edges[i].weight;
+    edges[i].weight = edges[j].weight;
+    edges[j].weight = temp;
 }
 
 /* 对权值进行排序 */
-void sort(Edge edges[],MGraph *G)
+void sort(Edge edges[], MGraph *G)
 {
-	int i, j;
-	for ( i = 0; i < G->numEdges; i++)
-	{
-		for ( j = i + 1; j < G->numEdges; j++)
-		{
-			if (edges[i].weight > edges[j].weight)
-			{
-				Swapn(edges, i, j);
-			}
-		}
-	}
-	printf("权排序之后的为:\n");
-	for (i = 0; i < G->numEdges; i++)
-	{
-		printf("(%d, %d) %d\n", edges[i].begin, edges[i].end, edges[i].weight);
-	}
-
+    int i, j;
+    for (i = 0; i < G->numEdges; i++)
+    {
+        for (j = i + 1; j < G->numEdges; j++)
+        {
+            if (edges[i].weight > edges[j].weight)
+            {
+                Swapn(edges, i, j);
+            }
+        }
+    }
+    printf("权排序之后的为:\n");
+    for (i = 0; i < G->numEdges; i++)
+    {
+        printf("(%d, %d) %d\n", edges[i].begin, edges[i].end, edges[i].weight);
+    }
 }
 
 /* 查找连线顶点的尾部下标 */
 int Find(int *parent, int f)
 {
-	while ( parent[f] > 0)
-	{
-		f = parent[f];
-	}
-	return f;
+    while (parent[f] > 0)
+    {
+        f = parent[f];
+    }
+    return f;
 }
 
 /* 生成最小生成树 */
 void MiniSpanTree_Kruskal(MGraph G)
 {
-	int i, j, n, m;
-	int k = 0;
-	int parent[MAXVEX];/* 定义一数组用来判断边与边是否形成环路 */
-	
-	Edge edges[MAXEDGE];/* 定义边集数组,edge的结构为begin,end,weight,均为整型 */
+    int i, j, n, m;
+    int k = 0;
+    int parent[MAXVEX]; /* 定义一数组用来判断边与边是否形成环路 */
 
-	/* 用来构建边集数组并排序********************* */
-	for ( i = 0; i < G.numVertexes-1; i++)
-	{
-		for (j = i + 1; j < G.numVertexes; j++)
-		{
-			if (G.arc[i][j]<GRAPH_INFINITY)
-			{
-				edges[k].begin = i;
-				edges[k].end = j;
-				edges[k].weight = G.arc[i][j];
-				k++;
-			}
-		}
-	}
-	sort(edges, &G);
-	/* ******************************************* */
+    Edge edges[MAXEDGE]; /* 定义边集数组,edge的结构为begin,end,weight,均为整型 */
 
+    /* 用来构建边集数组并排序********************* */
+    for (i = 0; i < G.numVertexes - 1; i++)
+    {
+        for (j = i + 1; j < G.numVertexes; j++)
+        {
+            if (G.arc[i][j] < GRAPH_INFINITY)
+            {
+                edges[k].begin = i;
+                edges[k].end = j;
+                edges[k].weight = G.arc[i][j];
+                k++;
+            }
+        }
+    }
+    sort(edges, &G);
+    /* ******************************************* */
 
-	for (i = 0; i < G.numVertexes; i++)
-		parent[i] = 0;	/* 初始化数组值为0 */
+    for (i = 0; i < G.numVertexes; i++)
+        parent[i] = 0; /* 初始化数组值为0 */
 
-	printf("打印最小生成树：\n");
-	for (i = 0; i < G.numEdges; i++)	/* 循环每一条边 */
-	{
-		n = Find(parent,edges[i].begin);
-		m = Find(parent,edges[i].end);
-		if (n != m) /* 假如n与m不等，说明此边没有与现有的生成树形成环路 */
-		{
-			parent[n] = m;	/* 将此边的结尾顶点放入下标为起点的parent中。 */
-							/* 表示此顶点已经在生成树集合中 */
-			printf("(%d, %d) %d\n", edges[i].begin, edges[i].end, edges[i].weight);
-		}
-	}
+    printf("打印最小生成树：\n");
+    for (i = 0; i < G.numEdges; i++) /* 循环每一条边 */
+    {
+        n = Find(parent, edges[i].begin);
+        m = Find(parent, edges[i].end);
+        if (n != m) /* 假如n与m不等，说明此边没有与现有的生成树形成环路 */
+        {
+            parent[n] = m; /* 将此边的结尾顶点放入下标为起点的parent中。 */
+            /* 表示此顶点已经在生成树集合中 */
+            printf("(%d, %d) %d\n", edges[i].begin, edges[i].end, edges[i].weight);
+        }
+    }
 }
 
 int main(void)
 {
-	MGraph G;
-	CreateMGraph(&G);
-	MiniSpanTree_Kruskal(G);
-	return 0;
+    MGraph G;
+    CreateMGraph(&G);
+    MiniSpanTree_Kruskal(G);
+    return 0;
 }
--- Algorithm/Book/大话数据结构 溢彩加强版/tmp/第8章查找/05散列表_HashTable_UTF-8.c
+++ Algorithm/Book/大话数据结构 溢彩加强版/tmp/第8章查找/05散列表_HashTable_UTF-8.c
@@ -1,7 +1,7 @@
-#include "stdio.h"    
-#include "stdlib.h"   
+#include "stdio.h"
+#include "stdlib.h"
 
-#include "math.h"  
+#include "math.h"
 #include "time.h"
 
 #define OK 1
@@ -14,84 +14,84 @@
 #define SUCCESS 1
 #define UNSUCCESS 0
 #define HASHSIZE 12 /* 定义散列表长为数组的长度 */
-#define NULLKEY -32768 
+#define NULLKEY -32768
 
-typedef int Status;	/* Status是函数的类型,其值是函数结果状态代码，如OK等 */ 
+typedef int Status; /* Status是函数的类型,其值是函数结果状态代码，如OK等 */
 
 typedef struct
 {
-   int *elem; /* 数据元素存储基址，动态分配数组 */
-   int count; /*  当前数据元素个数 */
-}HashTable;
+    int *elem; /* 数据元素存储基址，动态分配数组 */
+    int count; /*  当前数据元素个数 */
+} HashTable;
 
-int m=0; /* 散列表表长，全局变量 */
+int m = 0; /* 散列表表长，全局变量 */
 
 /* 初始化散列表 */
 Status InitHashTable(HashTable *H)
 {
-	int i;
-	m=HASHSIZE;
-	H->count=m;
-	H->elem=(int *)malloc(m*sizeof(int));
-	for(i=0;i<m;i++)
-		H->elem[i]=NULLKEY; 
-	return OK;
+    int i;
+    m = HASHSIZE;
+    H->count = m;
+    H->elem = (int *)malloc(m * sizeof(int));
+    for (i = 0; i < m; i++)
+        H->elem[i] = NULLKEY;
+    return OK;
 }
 
 /* 散列函数 */
 int Hash(int key)
 {
-	return key % m; /* 除留余数法 */
+    return key % m; /* 除留余数法 */
 }
 
 /* 插入关键字进散列表 */
-void InsertHash(HashTable *H,int key)
+void InsertHash(HashTable *H, int key)
 {
-	int addr = Hash(key); /* 求散列地址 */
-	while (H->elem[addr] != NULLKEY) /* 如果不为空，则冲突 */
-	{
-		addr = (addr+1) % m; /* 开放定址法的线性探测 */
-	}
-	H->elem[addr] = key; /* 直到有空位后插入关键字 */
+    int addr = Hash(key);            /* 求散列地址 */
+    while (H->elem[addr] != NULLKEY) /* 如果不为空，则冲突 */
+    {
+        addr = (addr + 1) % m; /* 开放定址法的线性探测 */
+    }
+    H->elem[addr] = key; /* 直到有空位后插入关键字 */
 }
 
 /* 散列表查找关键字 */
-Status SearchHash(HashTable H,int key,int *addr)
+Status SearchHash(HashTable H, int key, int *addr)
 {
-	*addr = Hash(key);  /* 求散列地址 */
-	while(H.elem[*addr] != key) /* 如果不为空，则冲突 */
-	{
-		*addr = (*addr+1) % m; /* 开放定址法的线性探测 */
-		if (H.elem[*addr] == NULLKEY || *addr == Hash(key)) /* 如果循环回到原点 */
-			return UNSUCCESS;	/* 则说明关键字不存在 */
-	}
-	return SUCCESS;
+    *addr = Hash(key);           /* 求散列地址 */
+    while (H.elem[*addr] != key) /* 如果不为空，则冲突 */
+    {
+        *addr = (*addr + 1) % m;                            /* 开放定址法的线性探测 */
+        if (H.elem[*addr] == NULLKEY || *addr == Hash(key)) /* 如果循环回到原点 */
+            return UNSUCCESS;                               /* 则说明关键字不存在 */
+    }
+    return SUCCESS;
 }
 
 int main()
 {
-	int arr[HASHSIZE]={12,67,56,16,25,37,22,29,15,47,48,34};
-	int i,p,key,result;
-	HashTable H;
+    int arr[HASHSIZE] = {12, 67, 56, 16, 25, 37, 22, 29, 15, 47, 48, 34};
+    int i, p, key, result;
+    HashTable H;
 
-	key=39;
+    key = 39;
 
-	InitHashTable(&H);
-	for(i=0;i<m;i++)
-		 InsertHash(&H,arr[i]);
-	
-	result=SearchHash(H,key,&p);
-	if (result)
-		printf("查找 %d 的地址为：%d \n",key,p);
-	else
-		printf("查找 %d 失败。\n",key);
+    InitHashTable(&H);
+    for (i = 0; i < m; i++)
+        InsertHash(&H, arr[i]);
 
-	for(i=0;i<m;i++)
-	{
-		key=arr[i];
-		SearchHash(H,key,&p);
-		printf("查找 %d 的地址为：%d \n",key,p);
-	}
+    result = SearchHash(H, key, &p);
+    if (result)
+        printf("查找 %d 的地址为：%d \n", key, p);
+    else
+        printf("查找 %d 失败。\n", key);
 
-	return 0;
+    for (i = 0; i < m; i++)
+    {
+        key = arr[i];
+        SearchHash(H, key, &p);
+        printf("查找 %d 的地址为：%d \n", key, p);
+    }
+
+    return 0;
 }
