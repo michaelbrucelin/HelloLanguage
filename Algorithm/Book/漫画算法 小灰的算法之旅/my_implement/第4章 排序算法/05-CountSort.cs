e1425546b6586020acdd8fb23303213f1c656412
michaelbrucelin <michaelbrucelin@gmail.com>
2022-06-13 11:01:10 +0800

    20220613-110110

--- Algorithm/Book/漫画算法 小灰的算法之旅/my_implement/第4章 排序算法/05-CountSort.cs
+++ Algorithm/Book/漫画算法 小灰的算法之旅/my_implement/第4章 排序算法/05-CountSort.cs
@@ -17,16 +17,23 @@ namespace TestCSharp
             for (int i = 0; i < list.Count; i++)
                 Console.Write($"{list[i]}, ");
 
-            var list2 = CountSort(list);
+            var list2 = CountSort2(list);
 
             Console.WriteLine();
             for (int i = 0; i < list2.Count; i++)
                 Console.Write($"{list2[i]}, ");
         }
 
+        /// <summary>
+        /// 计数排序
+        /// 不稳定排序版本
+        /// 计数排序只适用于整数序列，而且序列中的最大值与最小值的差距不是很大的场景
+        /// </summary>
+        /// <param name="list"></param>
+        /// <returns></returns>
         public static IList<int> CountSort(IList<int> list)
         {
-            int min = list[0], max = list[list.Count - 1];
+            int min = list[0], max = list[0];
             foreach (int i in list)
             {
                 if (i < min)
@@ -36,19 +43,62 @@ namespace TestCSharp
             }
 
             int distance = max - min;
-            int[] temp = new int[distance + 1];  // C#中整型数组默认全为0，不需要初始化
+            int[] count_arr = new int[distance + 1];  // C#中整型数组默认全为0，不需要初始化
             foreach (int i in list)
             {
-                temp[i - min]++;
+                count_arr[i - min]++;
             }
 
             IList<int> result = new List<int>(list.Count);
-            for (int i = 0; i < temp.Length; i++)
+            for (int i = 0; i < count_arr.Length; i++)
             {
-                for (int j = 0; j < temp[i]; j++)
+                for (int j = 0; j < count_arr[i]; j++)
                 {
                     result.Add(min + i);
                 }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// 计数排序
+        /// 稳定排序版本
+        /// 非稳定排序版本中的count_arr，每一个元素表示原素组中这个位置对应的值一共有多少个
+        /// 此版本中的count_arr，每一个元素表示原数组中这个位置对应的值前面一共有多少（N）个值（准确说是N+1）
+        /// </summary>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        public static IList<int> CountSort2(IList<int> list)
+        {
+            int min = list[0], max = list[0];
+            foreach (int i in list)
+            {
+                if (i < min)
+                    min = i;
+                else if (i > max)
+                    max = i;
+            }
+
+            int distance = max - min;
+            int[] count_arr = new int[distance + 1];    // C#中整型数组默认全为0，不需要初始化
+            foreach (int i in list)
+            {
+                count_arr[i - min]++;
+            }
+
+            for (int i = 1; i < count_arr.Length; i++)  // 重新构建count_arr，以实现稳定排序
+            {
+                count_arr[i] += count_arr[i - 1];
+            }
+
+            IList<int> result = new List<int>(list.Count);
+            for (int i = 0; i < list.Count; i++) result.Add(int.MinValue);  // 初始化，否则下面只能使用Add()，而无法使用索引
+
+            for (int i = list.Count - 1; i >= 0; i--)
+            {
+                result[count_arr[list[i] - min] - 1] = list[i];
+                count_arr[list[i] - min]--;
             }
 
             return result;
---
+++ Algorithm/Book/漫画算法 小灰的算法之旅/my_implement/第4章 排序算法/06-BucketSort.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestCSharp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Random random = new Random();
+            List<decimal> list = new List<decimal>(32);
+            Parallel.For(0, 32, i => list.Add(random.Next(0, 100) * 1.0m / random.Next(1, 100)));
+
+            for (int i = 0; i < list.Count; i++)
+                Console.Write($"{list[i].ToString("0.00")}, ");
+
+            var list2 = BucketSort(list);
+
+            Console.WriteLine();
+            for (int i = 0; i < list2.Count; i++)
+                Console.Write($"{list2[i].ToString("0.00")}, ");
+        }
+
+        /// <summary>
+        /// 桶排序
+        /// 这里采用有多少个元素，就创建多少个桶
+        /// 桶的跨度 = (最大值-最小值)/(桶数-1)，其中最后一个桶只存最大值，为[ ]，其余所有桶均为[ )
+        /// 之所以这么设计桶，因为代码更统一，更容易找到每个元素属于那个桶
+        /// </summary>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        public static IList<decimal> BucketSort(IList<decimal> list)
+        {
+            decimal min = list[0], max = list[0];
+            foreach (decimal i in list)
+            {
+                if (i < min)
+                    min = i;
+                else if (i > max)
+                    max = i;
+            }
+
+            // 初始化桶并将每一个元素分配到对应的桶中
+            decimal span = (max - min) / (list.Count - 1);
+            List<decimal>[] buckets = new List<decimal>[list.Count];
+            for (int i = 0; i < buckets.Length; i++) buckets[i] = new List<decimal>();
+            for (int i = 0; i < list.Count; i++)
+            {
+                buckets[(int)Math.Floor(list[i] / span)].Add(list[i]);
+            }
+
+            for (int i = 0; i < buckets.Length; i++)
+            {
+                buckets[i].Sort();  // 每一个桶内部排序，这里直接使用API来实现
+            }
+
+            IList<decimal> result = new List<decimal>(list.Count);
+            for (int i = 0; i < buckets.Length; i++)
+            {
+                for (int j = 0; j < buckets[i].Count; j++)
+                {
+                    result.Add(buckets[i][j]);
+                }
+            }
+
+            return result;
+        }
+    }
+}
