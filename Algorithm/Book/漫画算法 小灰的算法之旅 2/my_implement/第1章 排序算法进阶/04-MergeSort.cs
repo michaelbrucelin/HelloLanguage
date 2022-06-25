648a56f3516312673e6b33f27330ab80f53acec2
michaelbrucelin <michaelbrucelin@gmail.com>
2022-06-25 16:29:59 +0800

    20220625-162959

--- Algorithm/Book/漫画算法 小灰的算法之旅 2/my_implement/第1章 排序算法进阶/04-MergeSort.cs
+++ Algorithm/Book/漫画算法 小灰的算法之旅 2/my_implement/第1章 排序算法进阶/04-MergeSort.cs
@@ -11,17 +11,17 @@ namespace TestCSharp
         public static void Main(string[] args)
         {
             Random random = new Random();
-            List<int> list = new List<int>(32);
-            Parallel.For(0, 32, i => list.Add(random.Next(0, 100)));
+            List<int> list = new List<int>(new int[random.Next(29, 43)]);
+            Parallel.For(0, list.Count, i => list[i] = random.Next(0, 100));
 
             for (int i = 0; i < list.Count; i++)
                 Console.Write($"{list[i]}, ");
 
-            var list2 = MergeSort2(list, 0, list.Count - 1);
+            MergeSort2(list);
 
             Console.WriteLine();
-            for (int i = 0; i < list2.Count; i++)
-                Console.Write($"{list2[i]}, ");
+            for (int i = 0; i < list.Count; i++)
+                Console.Write($"{list[i]}, ");
         }
 
         /// <summary>
@@ -32,49 +32,102 @@ namespace TestCSharp
         /// <param name="list"></param>
         /// <param name="start"></param>
         /// <param name="stop"></param>
-        public static IList<int> MergeSort(IList<int> list, int start, int stop)
+        public static void MergeSort(IList<int> list)
         {
-            if (start >= stop)
-                return list.Skip(start).Take(stop - start + 1).ToList();
+            MergeSort_(list, 0, list.Count - 1);
+        }
 
-            IList<int> left = MergeSort(list, start, (start + stop) / 2);
-            IList<int> right = MergeSort(list, (start + stop) / 2 + 1, stop);
+        private static void MergeSort_(IList<int> list, int start, int stop)
+        {
+            if (start >= stop) return;
 
-            IList<int> result = new List<int>(stop - start + 1);
-            int i = 0, j = 0;
-            while (i < left.Count && j < right.Count)
+            int mid = (start + stop) / 2;
+            MergeSort_(list, start, mid);
+            MergeSort_(list, mid + 1, stop);
+
+            IList<int> buffer = new List<int>(stop - start + 1);
+            int i = start, j = mid + 1;
+            while (i <= mid && j <= stop)
             {
-                if (left[i] <= right[j])
-                    result.Add(left[i++]);   // result[i + j] = left[i]; i++;
-                else                         // left[i] > right[j]
-                    result.Add(right[j++]);  // result[i + j] = right[j]; j++;
+                if (list[i] <= list[j])
+                    buffer.Add(list[i++]);
+                else
+                    buffer.Add(list[j++]);
             }
-            while (i < left.Count)
-                result.Add(left[i++]);       // result[i + j] = left[i]; i++;
-            while (j < right.Count)
-                result.Add(right[j++]);      // result[i + j] = right[j]; j++;
+            while (i <= mid)
+                buffer.Add(list[i++]);
+            while (j <= stop)
+                buffer.Add(list[j++]);
 
-            return result;
+            for (i = start, j = 0; i <= stop; i++, j++)
+                list[i] = buffer[j];
         }
 
         /// <summary>
         /// 归并排序
         /// 
         /// 非递归版，从前向后两两分组进行归并
+        /// span 0  1  2  3  4  5  6  7  8  9  10  11  12  13  14  15  16  17  18
+        ///   2  ----  ----  ----  ----  ----  ------  ------  ------  ------  --
+        ///   4  ----------  ----------  ------------  --------------  ----------
+        ///   8  ----------------------------------------------------  ----------
+        ///  16  ----------------------------------------------------------------
         /// </summary>
         /// <param name="list"></param>
         /// <returns></returns>
-        public static IList<int> MergeSort2(IList<int> list)
+        public static void MergeSort2(IList<int> list)
         {
-            int span = 2;
-            while (span < list.Count - 1)
+            int span = 1;
+            while ((span <<= 1) <= list.Count)
             {
-                for(int i = 0;i+span<list.Count;)
+                for (int step = 0; step < list.Count; step += span)
+                {
+                    int mid = step + (span >> 1);      // span中第2个有序序列的第1个元素的索引
+                    int border;                        // span中第2个有序序列的最后1个元素的索引
+                    if (step + span - 1 < list.Count)  // 最后1个span是完整的
+                        border = step + span - 1;
+                    else if (mid < list.Count)         // 最后1个span虽然不是完整的，但仍然需要合并
+                        border = list.Count - 1;
+                    else
+                        break;
 
-                span <<= 1;
+                    List<int> buffer = new List<int>(span);
+                    int i = step, j = mid;
+                    while (i < mid && j <= border)
+                    {
+                        if (list[i] <= list[j])
+                            buffer.Add(list[i++]);
+                        else
+                            buffer.Add(list[j++]);
+                    }
+                    while (i < mid)
+                        buffer.Add(list[i++]);
+
+                    while (j <= border)
+                        buffer.Add(list[j++]);
+
+                    for (i = step, j = 0; i <= border; i++, j++)
+                        list[i] = buffer[j];
+                }
             }
 
-            return null;
+            List<int> buffer_ = new List<int>(list.Count);
+            int mid_ = span >> 1, i_ = 0, j_ = mid_;
+            while (i_ < mid_ && j_ < list.Count)
+            {
+                if (list[i_] <= list[j_])
+                    buffer_.Add(list[i_++]);
+                else
+                    buffer_.Add(list[j_++]);
+            }
+            while (i_ < mid_)
+                buffer_.Add(list[i_++]);
+
+            while (j_ < list.Count)
+                buffer_.Add(list[j_++]);
+
+            for (i_ = 0; i_ < list.Count; i_++)
+                list[i_] = buffer_[i_];
         }
 
         private static void swap(IList<int> list, int i, int j)
