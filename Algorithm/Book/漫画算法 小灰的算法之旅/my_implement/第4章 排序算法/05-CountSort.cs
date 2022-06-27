348a85061b6c5bee47b50effece023414b14733d
michaelbrucelin <michaelbrucelin@gmail.com>
2022-06-27 09:04:28 +0800

    20220627-090428

--- Algorithm/Book/漫画算法 小灰的算法之旅/my_implement/第4章 排序算法/05-CountSort.cs
+++ Algorithm/Book/漫画算法 小灰的算法之旅/my_implement/第4章 排序算法/05-CountSort.cs
@@ -11,30 +11,30 @@ namespace TestCSharp
         public static void Main(string[] args)
         {
             Random random = new Random();
-            List<int> list = new List<int>(32);
-            Parallel.For(0, 32, i => list.Add(random.Next(0, 100)));
+            int[] arr = new int[random.Next(29, 43)];
+            Parallel.For(0, arr.Length, i => arr[i] = random.Next(0, 100));
 
-            for (int i = 0; i < list.Count; i++)
-                Console.Write($"{list[i]}, ");
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write($"{arr[i]}, ");
 
-            var list2 = CountSort2(list);
+            CountSort2(arr);
 
             Console.WriteLine();
-            for (int i = 0; i < list2.Count; i++)
-                Console.Write($"{list2[i]}, ");
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write($"{arr[i]}, ");
         }
+
 
         /// <summary>
         /// 计数排序
         /// 不稳定排序版本
         /// 计数排序只适用于整数序列，而且序列中的最大值与最小值的差距不是很大的场景
         /// </summary>
-        /// <param name="list"></param>
-        /// <returns></returns>
-        public static IList<int> CountSort(IList<int> list)
+        /// <param name="arr"></param>
+        public static void CountSort(int[] arr)
         {
-            int min = list[0], max = list[0];
-            foreach (int i in list)
+            int min = arr[0], max = arr[0];
+            foreach (int i in arr)
             {
                 if (i < min)
                     min = i;
@@ -44,48 +44,51 @@ namespace TestCSharp
 
             int distance = max - min;
             int[] count_arr = new int[distance + 1];  // C#中整型数组默认全为0，不需要初始化
-            foreach (int i in list)
+            foreach (int i in arr)
             {
                 count_arr[i - min]++;
             }
 
-            IList<int> result = new List<int>(list.Count);
+            int index = 0;
             for (int i = 0; i < count_arr.Length; i++)
             {
                 for (int j = 0; j < count_arr[i]; j++)
                 {
-                    result.Add(min + i);
+                    arr[index++] = (min + i);
                 }
             }
-
-            return result;
         }
 
         /// <summary>
         /// 计数排序
         /// 稳定排序版本
-        /// 非稳定排序版本中的count_arr，每一个元素表示原素组中这个位置对应的值一共有多少个
-        /// 此版本中的count_arr，每一个元素表示原数组中这个位置对应的值前面一共有多少（N）个值（准确说是N+1）
+        /// 非稳定排序版本中的count_arr，每一个元素表示原数组中这个位置对应的值一共有多少个
+        /// 此版本中的count_arr，每一个元素表示原数组中这个位置对应的值前面一共有多少（N）个值（准确说是N-1）
+        /// 例如原数组：
+        /// [95, 94, 91, 98, 99, 90, 99, 93, 91, 92]
+        /// [0   1   2   3   4   5   6   7   8   9 ]
+        /// 对应的count_arr：
+        /// [1   2   1   1   1   1   0   0   1   2 ]
+        /// [1   3   4   5   6   7   7   7   8   10 ]
         /// </summary>
-        /// <param name="list"></param>
-        /// <returns></returns>
-        public static IList<int> CountSort2(IList<int> list)
+        /// <param name="arr"></param>
+        public static void CountSort2(int[] arr)
         {
-            if (list.Count == 1) return list;
+            if (arr.Length == 1) return;
 
-            int min = list[0], max = list[0];
-            foreach (int i in list)
+            int min = arr[0], max = arr[0];
+            foreach (int i in arr)
             {
                 if (i < min)
                     min = i;
                 else if (i > max)
                     max = i;
             }
-            if (min == max) return list;
+            if (min == max) return;
 
             int distance = max - min;
             int[] count_arr = new int[distance + 1];    // C#中整型数组默认全为0，不需要初始化
-            foreach (int i in list)
+            foreach (int i in arr)
             {
                 count_arr[i - min]++;
             }
@@ -95,16 +98,16 @@ namespace TestCSharp
                 count_arr[i] += count_arr[i - 1];
             }
 
-            IList<int> result = new List<int>(list.Count);
-            for (int i = 0; i < list.Count; i++) result.Add(int.MinValue);  // 初始化，否则下面只能使用Add()，而无法使用索引
+            int[] buffer = new int[arr.Length];
 
-            for (int i = list.Count - 1; i >= 0; i--)
+            for (int i = arr.Length - 1; i >= 0; i--)
             {
-                result[count_arr[list[i] - min] - 1] = list[i];
-                count_arr[list[i] - min]--;
+                buffer[count_arr[arr[i] - min] - 1] = arr[i];
+                count_arr[arr[i] - min]--;
             }
 
-            return result;
+            for (int i = 0; i < arr.Length; i++)
+                arr[i] = buffer[i];
         }
     }
 }
