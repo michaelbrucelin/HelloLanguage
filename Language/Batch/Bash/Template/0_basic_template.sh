c3956cfc1b1dc36351369ffdda2e1c9027e76d3e
michaelbrucelin <michaelbrucelin@gmail.com>
2022-06-30 11:47:23 +0800

    20220630-114723

--- Language/Batch/Bash/Template/0_basic_template.sh
+++ Language/Batch/Bash/Template/0_basic_template.sh
@@ -29,19 +29,19 @@ set +e
 
 : '
 shell script基本框架
-1、第一行#!/bin/bash声明这个script使用的shell名称；
-2、整个script中，除了第一行的#!用来声明shell之外，其余的#都是注释；
-3、声明主要的环境变量，主要是PATH和LANG；
-4、程序内容；
-5、执行结果；
+1. 第一行#!/bin/bash声明这个script使用的shell名称；
+2. 整个script中，除了第一行的#!用来声明shell之外，其余的#都是注释；
+3. 声明主要的环境变量，主要是PATH和LANG；
+4. 程序内容；
+5. 执行结果；
 
 shell script好的编写习惯
 在script文件头处记录好：
-1、script的功能；
-2、script的版本信息；
-3、script的作者与联络方式；
-4、script的版权声明方式；
-5、script的history；
-script内使用比较特殊的命令，使用绝对路径的方式去执行；
-script内预先声明执行需要的环境变量；
+1. script的功能；
+2. script的版本信息；
+3. script的作者与联络方式；
+4. script的版权声明方式；
+5. script的history；
+6. script内使用比较特殊的命令，使用绝对路径的方式去执行；
+7. script内预先声明执行需要的环境变量；
 '
