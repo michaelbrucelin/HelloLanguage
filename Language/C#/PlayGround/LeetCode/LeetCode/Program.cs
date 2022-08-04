8348c54ad71f482088642be7ace7f617c078a839
michael brucelin <michaelbrucelin@gmail.com>
2022-08-04 11:02:33 +0800

    20220804-1102

---
+++ Language/C#/PlayGround/LeetCode/LeetCode/Program.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode.QuestionBank;
+using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Question1403 question = new Question1403();
+            question.Test();
+        }
+    }
+}
---
+++ Language/C#/PlayGround/LeetCode/LeetCode/QuestionBank/Question1403.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank
+{
+    public class Question1403
+    {
+        public void Test()
+        {
+            Question1403 q1403 = new Question1403();
+
+            int[] nums = new int[] { 4, 3, 10, 9, 8 };
+            IList<int> result = q1403.MinSubsequence(nums);
+            foreach (int i in result)
+                Console.Write($"{i} ");
+
+            Console.WriteLine();
+            nums = new int[] { 4, 4, 7, 6, 7 };
+            result = q1403.MinSubsequence(nums);
+            foreach (int i in result)
+                Console.Write($"{i} ");
+        }
+
+        public IList<int> MinSubsequence(int[] nums)
+        {
+            List<int> listTar = new List<int>();
+
+            int sumsrc = 0, sumtar = 0;
+            List<int> listSrc = nums.ToList();
+            sumsrc = nums.Sum();
+
+            while (sumtar <= sumsrc)
+            {
+                int maxid = 0;
+                for (int i = 0; i < listSrc.Count; i++)
+                    if (listSrc[i] > listSrc[maxid])
+                        maxid = i;
+
+                sumtar += listSrc[maxid];
+                sumsrc -= listSrc[maxid];
+                listTar.Add(listSrc[maxid]);
+                listSrc.RemoveAt(maxid);
+            }
+
+            return listTar;
+        }
+    }
+}
