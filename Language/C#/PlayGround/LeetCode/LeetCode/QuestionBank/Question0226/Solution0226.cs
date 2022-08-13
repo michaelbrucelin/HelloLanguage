6f1f63c00256691f6e0298a1d8e664c144e4230b
michael brucelin <michaelbrucelin@gmail.com>
2022-08-13 17:39:18 +0800

    '20220813-1739'

---
+++ Language/C#/PlayGround/LeetCode/LeetCode/QuestionBank/Question0226/Solution0226.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question0226
+{
+    public class Solution0226
+    {
+        public TreeNode InvertTree(TreeNode root)
+        {
+            if (root == null) return root;
+
+            TreeNode left = InvertTree(root.left);
+            TreeNode right = InvertTree(root.right);
+
+            root.left = right;
+            root.right = left;
+
+            return root;
+        }
+    }
+}
