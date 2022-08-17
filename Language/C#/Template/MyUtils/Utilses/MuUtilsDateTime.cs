3bdea020f87f4021af1a5deb2525a199dcf7cf78
michael brucelin <michaelbrucelin@gmail.com>
2022-08-17 10:18:13 +0800

    '20220817-1018'

--- Language/C#/Template/MyUtils/Utilses/MuUtilsDateTime.cs
+++ Language/C#/Template/MyUtils/Utilses/MuUtilsDateTime.cs
@@ -4,16 +4,62 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TestCSharp
+namespace WindowsFormsApp0.Classes.Utilses
 {
-    public static class QUtilsDateTime
+    public static class MyUtilsDateTime
     {
         private static readonly DateTime Epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
 
-        public static long ConvertToTimestamp(DateTime value)
+        /// <summary>
+        /// 将DateTime转为Unix时间戳
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static long ConvertToUnixTimeStamp(DateTime value)
         {
             TimeSpan elapsedTime = value - Epoch;
             return (long)elapsedTime.TotalSeconds;
+        }
+
+        /// <summary>
+        /// 将DateTime转为Java时间戳
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static long ConvertToJavaTimeStamp(DateTime value)
+        {
+            TimeSpan elapsedTime = value - Epoch;
+            return (long)elapsedTime.TotalMilliseconds;
+        }
+
+        /// <summary>
+        /// 将Unix时间戳转为DateTime
+        /// </summary>
+        /// <param name="unixTimeStamp"></param>
+        /// <returns></returns>
+        public static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
+        {
+            // Unix timestamp is seconds past epoch
+            DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            // dateTime = dateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+            dateTime = dateTime.AddSeconds(unixTimeStamp).ToUniversalTime();
+
+            return dateTime;
+        }
+
+        /// <summary>
+        /// 将Java时间戳转为DateTime
+        /// </summary>
+        /// <param name="javaTimeStamp"></param>
+        /// <returns></returns>
+        public static DateTime JavaTimeStampToDateTime(double javaTimeStamp)
+        {
+            // Java timestamp is milliseconds past epoch
+            DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            // dateTime = dateTime.AddMilliseconds(javaTimeStamp).ToLocalTime();
+            dateTime = dateTime.AddMilliseconds(javaTimeStamp).ToUniversalTime();
+
+            return dateTime;
         }
     }
 }
