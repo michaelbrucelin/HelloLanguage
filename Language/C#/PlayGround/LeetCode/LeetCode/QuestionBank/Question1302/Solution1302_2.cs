43b3340c6621e89607047e6d363e74d5945eccd4
michael brucelin <michaelbrucelin@gmail.com>
2022-08-17 10:00:21 +0800

    '20220817-1000'

---
+++ Language/C#/PlayGround/LeetCode/LeetCode/QuestionBank/Question1302/Solution1302_2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1302
+{
+    public class Solution1302_2
+    {
+        private int result = 0;
+        private int maxLevel = -1;
+
+        public int DeepestLeavesSum(TreeNode root)
+        {
+            DeepestLeavesSum_DFS(root, 1);
+
+            return result;
+        }
+
+        private void DeepestLeavesSum_DFS(TreeNode node, int level)
+        {
+            if (node == null) return;
+
+            if (level > maxLevel)
+            {
+                maxLevel = level;
+                result = node.val;
+            }
+            else if (level == maxLevel)
+                result += node.val;
+
+            DeepestLeavesSum_DFS(node.left, level + 1);
+            DeepestLeavesSum_DFS(node.right, level + 1);
+        }
+    }
+}
---
+++ Language/C#/PlayGround/LeetCode/LeetCode/QuestionBank/Question1302/Solution1302_3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1302
+{
+    public class Solution1302_3
+    {
+        public int DeepestLeavesSum(TreeNode root)
+        {
+            int result = 0;
+
+            Queue<TreeNode> buffer = new Queue<TreeNode>();
+            buffer.Enqueue(root);
+            while (buffer.Count > 0)
+            {
+                result = 0;
+                int levelcnt = buffer.Count;
+                while (levelcnt-- > 0)
+                {
+                    TreeNode node = buffer.Dequeue();
+                    result += node.val;
+                    if (node.left != null) buffer.Enqueue(node.left);
+                    if (node.right != null) buffer.Enqueue(node.right);
+                }
+            }
+
+            return result;
+        }
+    }
+}
