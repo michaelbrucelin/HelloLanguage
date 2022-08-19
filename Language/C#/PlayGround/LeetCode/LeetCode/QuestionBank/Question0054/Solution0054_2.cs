e8fe808868183cf6b27a266ecb80e170243d67bb
michael brucelin <michaelbrucelin@gmail.com>
2022-08-19 13:46:26 +0800

    '20220819-1346'

--- Language/C#/PlayGround/LeetCode/LeetCode/QuestionBank/Question0054/Solution0054_2.cs
+++ Language/C#/PlayGround/LeetCode/LeetCode/QuestionBank/Question0054/Solution0054_2.cs
@@ -13,11 +13,31 @@ namespace LeetCode.QuestionBank.Question0054
             int width = matrix[0].Length, height = matrix.Length;
             int[] result = new int[width * height];
 
-            bool[,] visited = new bool[height, width];
-            int index = 0, row = 0, column = 0;
+            bool[,] visited = new bool[height + 2, width + 2];  // 外围一圈为true，这样下面就不需要考虑边界问题了matrix[i,j]对应于visited[i+1,j+1]
+            for (int i = 0; i < width + 2; i++)
+            {
+                visited[0, i] = true;
+                visited[height + 1, i] = true;
+            }
+            for (int i = 1; i < height + 2; i++)
+            {
+                visited[i, 0] = true;
+                visited[i, width + 1] = true;
+            }
+
+            int index = 0;
+            (int x, int y) position = (0, 0);
+            int direction = 0;  // 0 左→右  1 上→下  2 右→左  3 下→上
+            int[,] directions = new int[4, 2] { { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 } };
             while (index < result.Length)
             {
+                result[index++] = matrix[position.y][position.x];
+                visited[position.y + 1, position.x + 1] = true;
 
+                if (visited[position.y + directions[direction, 1] + 1, position.x + directions[direction, 0] + 1])
+                    direction = (direction + 1) % 4;
+
+                position = (position.x + directions[direction, 0], position.y + directions[direction, 1]);
             }
 
             return result;
