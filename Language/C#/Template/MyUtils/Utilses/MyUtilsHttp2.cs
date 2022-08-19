71e8a1ee445b50d1b4c97fbf6fb234d3f5fb4e3a
michaelbrucelin <michaelbrucelin@gmail.com>
2022-08-19 17:37:49 +0800

    20220819-173749

--- Language/C#/Template/MyUtils/Utilses/MyUtilsHttp2.cs
+++ Language/C#/Template/MyUtils/Utilses/MyUtilsHttp2.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,11 +11,20 @@ namespace WindowsFormsApp0
 {
     public static class MyUtilsHttp2
     {
-        //https://stackoverflow.com/questions/4015324/how-to-make-http-post-web-request
+        static MyUtilsHttp2()
+        {
+            client.DefaultRequestHeaders.AcceptEncoding.Add(new StringWithQualityHeaderValue("gzip"));     // 允许服务端以gzip格式压缩数据
+            client.DefaultRequestHeaders.AcceptEncoding.Add(new StringWithQualityHeaderValue("deflate"));  // 允许服务端以deflate格式压缩数据
+        }
 
         #region HttpClient
-        private static readonly HttpClient client = new HttpClient();
+        private static HttpClientHandler handler = new HttpClientHandler()
+        {
+            AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate  // 自动解压缩
+        };
+        private static readonly HttpClient client = new HttpClient(handler);
 
+        #region 异步方法
         public static async Task<string> GETWithHttpClientAsync(string url)
         {
             string responseString = await client.GetStringAsync(url);
@@ -23,18 +34,81 @@ namespace WindowsFormsApp0
 
         public static async Task<string> PostWithHttpClientAsync(string url, Dictionary<string, string> values)
         {
-            //POST，在.Net Framework 4.5下测试有效，配合async/await很不错
-            //Dictionary<string, string> values = new Dictionary<string, string>()
-            //{
-            //    { "thing1", "hello" },
-            //    { "thing2", "world" }
-            //};
+            // Dictionary<string, string> values = new Dictionary<string, string>()
+            // {
+            //     { "thing1", "hello" },
+            //     { "thing2", "world" }
+            // };
+
+            // KeyValuePair<string, string>[] values = new KeyValuePair<string, string>[] {
+            //     new KeyValuePair<string, string>("thing1", "hello"),
+            //     new KeyValuePair<string, string>("thing2", "world")
+            // };
+
             FormUrlEncodedContent content = new FormUrlEncodedContent(values);
             HttpResponseMessage response = await client.PostAsync(url, content);
             string responseString = await response.Content.ReadAsStringAsync();
 
             return responseString;
         }
+
+        public static async Task<string> PostWithHttpClientAsync(string url, string json)
+        {
+            HttpRequestMessage message = new HttpRequestMessage(HttpMethod.Post, url);
+            message.Content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = await client.SendAsync(message);
+            string responseString = await response.Content.ReadAsStringAsync();
+
+            return responseString;
+        }
+        #endregion
+
+        #region 同步方法
+        // .Net Core 5中有同步方法，client.Send()，即client.SendAsync()的同步版本，从哪个版本开始有的不清楚，.Net Framework 4.8中没有同步方法
+        // 这里只能通过阻塞Task来实现
+        public static string GETWithHttpClient(string url)
+        {
+            Task<string> task = client.GetStringAsync(url);
+            task.Wait();
+
+            return task.Result;
+        }
+
+        public static string PostWithHttpClient(string url, Dictionary<string, string> values)
+        {
+            // Dictionary<string, string> values = new Dictionary<string, string>()
+            // {
+            //     { "thing1", "hello" },
+            //     { "thing2", "world" }
+            // };
+
+            // KeyValuePair<string, string>[] values = new KeyValuePair<string, string>[] {
+            //     new KeyValuePair<string, string>("thing1", "hello"),
+            //     new KeyValuePair<string, string>("thing2", "world")
+            // };
+
+            FormUrlEncodedContent content = new FormUrlEncodedContent(values);
+            Task<HttpResponseMessage> task1 = client.PostAsync(url, content);
+            task1.Wait();
+            Task<string> task2 = task1.Result.Content.ReadAsStringAsync();
+
+            return task2.Result;
+        }
+
+        public static string PostWithHttpClient(string url, string json)
+        {
+            HttpRequestMessage message = new HttpRequestMessage(HttpMethod.Post, url);
+            message.Content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            Task<HttpResponseMessage> task1 = client.SendAsync(message);
+            task1.Wait();
+            Task<string> task2 = task1.Result.Content.ReadAsStringAsync();
+            task2.Wait();
+
+            return task2.Result;
+        }
+        #endregion
         #endregion
     }
 }
