1663d3309d825179afca296ef9cee1cfcc1e66c0
mlin <491601759@qq.com>
2022-08-21 13:58:37 +0000

    20220821-215837

---
+++ Language/C#/PlayGround/LeetCode/LeetCode/QuestionBank/Question1455/Solution1455.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1455
+{
+    public class Solution1455
+    {
+        public int IsPrefixOfWord(string sentence, string searchWord)
+        {
+            string[] words = sentence.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].StartsWith(searchWord))
+                    return i + 1;
+            }
+            return -1;
+        }
+    }
+}
