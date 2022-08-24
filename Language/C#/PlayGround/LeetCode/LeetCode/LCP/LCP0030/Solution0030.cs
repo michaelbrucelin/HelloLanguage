fe60f8fc855e9241349527395a1c263924c650a6
michael brucelin <michaelbrucelin@gmail.com>
2022-08-24 12:00:36 +0800

    '20220824-1200'

---
+++ Language/C#/PlayGround/LeetCode/LeetCode/LCP/LCP0030/Solution0030.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.LCP.LCP0030
+{
+    public class Solution0030
+    {
+        public int MagicTower(int[] nums)
+        {
+            if (nums.Sum() < 0) return -1;
+
+            int result = 0;
+
+            int current = 1;
+            Queue<int> queue = new Queue<int>(nums);
+            Stack<int> stack = new Stack<int>();  // .NetFramework没有内建优先队列，这里用栈简单模拟
+            while (queue.Count > 0)
+            {
+                int value = queue.Dequeue();
+                current += value;
+                if (value < 0) _Push(stack, value);
+
+                while (current <= 0)
+                {
+                    if (stack.Count == 0) return -1;
+
+                    int recover = stack.Pop();
+                    current -= recover;
+                    queue.Enqueue(recover);
+                    result++;
+                }
+            }
+
+            return result;
+        }
+
+        private void _Push(Stack<int> stack, int i)
+        {
+            Stack<int> buffer = new Stack<int>();
+            while (stack.Count > 0 && stack.Peek() < i)
+                buffer.Push(stack.Pop());
+
+            stack.Push(i);
+            while (buffer.Count > 0)
+                stack.Push(buffer.Pop());
+        }
+    }
+}
---
+++ Language/C#/PlayGround/LeetCode/LeetCode/QuestionBank/Question1460/Solution1460.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1460
+{
+    public class Solution1460
+    {
+        /// <summary>
+        /// 只要两个数组中的元素完全相同，就一定有解
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="arr"></param>
+        /// <returns></returns>
+        public bool CanBeEqual(int[] target, int[] arr)
+        {
+            Dictionary<int, int> buffer = new Dictionary<int, int>();
+            for (int i = 0; i < target.Length; i++)
+            {
+                if (!buffer.ContainsKey(target[i]))
+                    buffer.Add(target[i], 1);
+                else
+                    buffer[target[i]]++;
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (!buffer.ContainsKey(arr[i])) return false;
+
+                if (buffer[arr[i]] == 1)
+                    buffer.Remove(arr[i]);
+                else
+                    buffer[arr[i]]--;
+            }
+
+            return true;
+        }
+    }
+}
