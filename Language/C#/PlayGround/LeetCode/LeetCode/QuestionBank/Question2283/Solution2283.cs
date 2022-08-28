fcf66e64de5bf10d9c181040fe06e1bdd64f79a1
mlin <491601759@qq.com>
2022-08-28 03:57:39 +0000

    20220828-1157

---
+++ Language/C#/PlayGround/LeetCode/LeetCode/QuestionBank/Question2283/Solution2283.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question2283
+{
+    public class Solution2283
+    {
+    }
+}
