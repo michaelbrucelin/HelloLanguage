a7ebaf78945cc8cc51e0e76a4247b69498341ff6
mlin <491601759@qq.com>
2022-08-28 12:33:30 +0000

    20220828-203329

--- Language/C#/PlayGround/LeetCode/LeetCode/QuestionBank/Question2283/Solution2283.cs
+++ Language/C#/PlayGround/LeetCode/LeetCode/QuestionBank/Question2283/Solution2283.cs
@@ -8,5 +8,16 @@ namespace LeetCode.QuestionBank.Question2283
 {
     public class Solution2283
     {
+        public bool DigitCount(string num)
+        {
+            Dictionary<int, int> buffer = new Dictionary<int, int>();
+            for (int i = 0; i < 10; i++) buffer.Add(i, 0);
+            for (int i = 0; i < num.Length; i++) buffer[num[i] - '0']++;
+
+            for (int i = 0; i < num.Length; i++)
+                if (num[i] - '0' != buffer[i]) return false;
+
+            return true;
+        }
     }
 }
