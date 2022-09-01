3729af5e1197045bfd11f31105cad5d16d22d5e2
michael brucelin <michaelbrucelin@gmail.com>
2022-09-01 09:10:23 +0800

    20220901-0910

---
+++ Language/C#/PlayGround/LeetCode/LeetCode/QuestionBank/Question1475/Solution1475.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1475
+{
+    public class Solution1475
+    {
+        public int[] FinalPrices(int[] prices)
+        {
+            int[] result = prices.ToArray();
+
+            for (int i = 0; i < prices.Length; i++)
+            {
+                for (int j = i + 1; j < prices.Length; j++)
+                {
+                    if (prices[j] <= prices[i])
+                    {
+                        result[i] -= prices[j];
+                        break;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Language/C#/PlayGround/LeetCode/LeetCode/QuestionBank/Question1475/Solution1475_2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1475
+{
+    public class Solution1475_2
+    {
+        public int[] FinalPrices(int[] prices)
+        {
+            int[] result = prices.ToArray();
+
+            Stack<int> stack = new Stack<int>();
+            for (int i = prices.Length - 1; i >= 0; i--)
+            {
+                while (stack.Count > 0 && stack.Peek() > prices[i]) stack.Pop();
+
+                if (stack.Count == 0)
+                {
+                    stack.Push(prices[i]);
+                    continue;
+                }
+
+                if (prices[i] == stack.Peek()) result[i] = 0;
+                else
+                {
+                    result[i] -= stack.Peek();
+                    stack.Push(prices[i]);
+                }
+            }
+
+            return result;
+        }
+    }
+}
