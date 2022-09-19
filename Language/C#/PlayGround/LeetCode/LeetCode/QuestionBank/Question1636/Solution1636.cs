a8302d56a78a911e335f049205c1c59be4d1e8cf
michael brucelin <michaelbrucelin@gmail.com>
2022-09-19 08:19:58 +0800

    20220919-0819

---
+++ Language/C#/PlayGround/LeetCode/LeetCode/QuestionBank/Question1636/Solution1636.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1636
+{
+    public class Solution1636 : Interface1636
+    {
+        /// <summary>
+        /// 不使用LINQ
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int[] FrequencySort(int[] nums)
+        {
+            Dictionary<int, int> helper = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (helper.ContainsKey(nums[i])) helper[nums[i]]++;
+                else helper.Add(nums[i], 1);
+            }
+
+            Array.Sort(nums, (i, j) =>
+            {
+                if (helper[i] != helper[j]) return helper[i] - helper[j];
+                else return j - i;
+            });
+
+            return nums;
+        }
+
+        /// <summary>
+        /// 使用LINQ
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int[] FrequencySort2(int[] nums)
+        {
+            return nums.OrderBy(i => nums.Count(j => j == i)).ThenByDescending(i => i).ToArray();
+        }
+    }
+}
