a8a7090e7949523af13e1756f729014071de4d6a
michael brucelin <michaelbrucelin@gmail.com>
2022-09-24 15:13:55 +0800

    20220924-1513

---
+++ Language/C#/PlayGround/LeetCode/LeetCode/QuestionBank/Question0011/Interface0011.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question0011
+{
+    public interface Interface0011
+    {
+        public int MaxArea(int[] height);
+    }
+}
