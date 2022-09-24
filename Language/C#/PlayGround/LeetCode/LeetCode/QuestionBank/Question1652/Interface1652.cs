1e97b1e025784177c21bb3edab628fca75eed501
michael brucelin <michaelbrucelin@gmail.com>
2022-09-24 09:38:28 +0800

    20220924-0938

---
+++ Language/C#/PlayGround/LeetCode/LeetCode/QuestionBank/Question1652/Interface1652.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1652
+{
+    public interface Interface1652
+    {
+        public int[] Decrypt(int[] code, int k);
+    }
+}
