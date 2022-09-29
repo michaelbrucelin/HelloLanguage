984afc25b0909070b28ba49b4ed444a1e9cf9294
michael brucelin <michaelbrucelin@gmail.com>
2022-09-29 09:13:59 +0800

    20220929-0913

---
+++ Language/C#/PlayGround/LeetCode/LeetCode/Interview/Interview0109/Solution0109.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Interview.Interview0109
+{
+    public class Solution0109 : Interface0109
+    {
+        public bool IsFlipedString(string s1, string s2)
+        {
+            return s1.Length == s2.Length && $"{s1}{s1}".Contains(s2);
+        }
+    }
+}
