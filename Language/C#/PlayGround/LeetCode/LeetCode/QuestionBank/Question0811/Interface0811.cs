f5595cf651ca58770e172ad431888d5555dc5f60
mlin <36387756+michaelbrucelin@users.noreply.github.com>
2022-10-05 17:46:59 +0800

    20221005-1746

---
+++ Language/C#/PlayGround/LeetCode/LeetCode/QuestionBank/Question0811/Interface0811.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question0811
+{
+    public interface Interface0811
+    {
+        public IList<string> SubdomainVisits(string[] cpdomains);
+    }
+}
