a95434320464b6e6334eae3665c4f47d1561b9d6
mlin <36387756+michaelbrucelin@users.noreply.github.com>
2022-10-07 08:49:29 +0800

    20221007-0849

---
+++ Language/C#/PlayGround/LeetCode/LeetCode/QuestionBank/Question1800/Interface1800.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1800
+{
+    public interface Interface1800
+    {
+        public int MaxAscendingSum(int[] nums);
+    }
+}
---
+++ Language/C#/PlayGround/LeetCode/LeetCode/QuestionBank/Question1800/Solution1800.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1800
+{
+    public class Solution1800 : Interface1800
+    {
+        public int MaxAscendingSum(int[] nums)
+        {
+            int result = 0;
+            int temp, id = 0;
+            while (id < nums.Length)
+            {
+                temp = nums[id];
+                while (++id < nums.Length && nums[id] > nums[id - 1]) temp += nums[id];
+                if (temp > result) result = temp;
+            }
+
+            return result;
+        }
+    }
+}
