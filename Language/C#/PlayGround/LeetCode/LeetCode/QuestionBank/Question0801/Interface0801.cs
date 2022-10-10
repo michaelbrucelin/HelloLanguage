021263f586f7268c34b400eecf8320c23bb9e2c4
unknown <Q1953@quickcomglobal.com>
2022-10-10 10:47:36 +0800

    20201010-1047

---
+++ Language/C#/PlayGround/LeetCode/LeetCode/QuestionBank/Question0801/Interface0801.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question0801
+{
+    public interface Interface0801
+    {
+        public int MinSwap(int[] nums1, int[] nums2);
+    }
+}
