1982bd74a41a3920a1deaf6421b48c845b780f75
unknown <Q1953@quickcomglobal.com>
2022-10-10 08:56:16 +0800

    20221010-0856

---
+++ Algorithm/Book/大话数据结构 溢彩加强版/my_implement_c/my_implement_c/01顺序栈_Stack.c
@@ -0,0 +1,42 @@
+/* 顺序栈结构 */
+#define MAXSIZE 20            // 存储空间初始分配量
+
+typedef int SElemType;        // SElemType类型根据实际情况而定，这里假设为int
+typedef struct
+{
+	SElemType data[MAXSIZE];
+	int top;                  // 用于栈顶指针
+}SqStack;
+
+#define OK 1
+#define ERROR 0
+
+typedef int Status;           // Status是函数的类型，其值是函数结果的状态代码，如OK等
+
+
+/*进栈操作*/
+// 插入元素e为新的栈顶元素
+Status Push(SqStack* S, SElemType e)
+{
+	if (S->top == MAXSIZE - 1)  // 栈满
+		return ERROR;
+
+	S->top++;
+	S->data[S->top] = e;
+
+	return OK;
+}
+
+
+/*出栈操作*/
+// 若栈不空，则删除S的栈顶元素，用e返回其值，并返回OK；否则返回ERROR
+Status Pop(SqStack* S, SElemType* e)
+{
+	if (S->top == -1)
+		return ERROR;
+
+	*e = S->data[S->top];  // 将要删除的栈顶元素赋值给e
+	S->top--;              // 栈顶指针减一
+
+	return OK;
+}
---
+++ Algorithm/Book/大话数据结构 溢彩加强版/my_implement_c/my_implement_c/03链栈_LinkStack.c
@@ -0,0 +1,69 @@
+#include <malloc.h>
+
+/* 链栈结构 */
+typedef int SElemType;        // SElemType类型根据实际情况而定，这里假设为int
+typedef struct StackNode
+{
+	SElemType data;
+	struct StackNode* next;
+}StackNode, * LinkStackPtr;
+
+typedef struct
+{
+	LinkStackPtr top;
+	int count;
+}LinkStack;
+
+#define OK 1
+#define ERROR 0
+#define TRUE 1
+#define FALSE 0
+
+typedef int Status;           // Status是函数的类型，其值是函数结果的状态代码，如OK等
+
+
+/*进栈操作*/
+// 插入元素e为新的栈顶元素
+Status Push(LinkStack* S, SElemType e)
+{
+	LinkStackPtr s = (LinkStackPtr)malloc(sizeof(StackNode));
+	if (s == NULL)
+	{
+		printf("Memory allocation failed");
+		return ERROR;
+	}
+
+	s->data = e;
+	s->next = S->top;  // 把当前的栈顶元素赋值给新结点的直接后继
+	S->top = s;        // 将新的结点s赋值给栈顶指针
+	S->count++;
+
+	return OK;
+}
+
+
+/*出栈操作*/
+// 若栈S为空栈，则返回TRUE，否则返回FALSE
+Status StackEmpty(LinkStack S)
+{
+	if (S.count == 0)
+		return TRUE;
+	else
+		return FALSE;
+}
+
+// 若栈不空，则删除S的栈顶元素，用e返回其值，并返回OK；否则返回ERROR
+Status Pop(LinkStack* S, SElemType* e)
+{
+	LinkStackPtr p;
+	if (StackEmpty(*S))
+		return ERROR;
+
+	*e = S->top->data;
+	p = S->top;
+	S->top = S->top->next;
+	free(p);
+	S->count--;
+
+	return OK;
+}
