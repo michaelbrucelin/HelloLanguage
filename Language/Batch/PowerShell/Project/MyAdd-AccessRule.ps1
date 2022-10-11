124c8501add4691181966e8206dec262aecd1945
michaelbrucelin <michaelbrucelin@gmail.com>
2022-10-11 16:28:26 +0800

    20221011-162826

---
+++ Language/Batch/PowerShell/Project/MyAdd-AccessRule.ps1
@@ -0,0 +1,25 @@
+# 递归查找所有文件夹，并添加权限
+# 场景：将一块装有操作系统的磁盘，挂载到另一台电脑上，想要删除磁盘上原C盘的文件，但是删不掉，需要将当前用户设置为有文件夹的全部权限，才可以删除文件夹
+
+function MyAdd-AccessRule() {
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$dir,                   # $dir = 'C:\Users\Public\'
+        [Parameter(Mandatory = $true)]
+        [string]$identity               # $identity = 'BUILTIN\User'
+    )
+
+    $rights = 'FullControl'                           # Other options: [enum]::GetValues('System.Security.AccessControl.FileSystemRights')
+    $inheritance = 'ContainerInherit, ObjectInherit'  # Other options: [enum]::GetValues('System.Security.AccessControl.Inheritance')
+    $propagation = 'None'                             # Other options: [enum]::GetValues('System.Security.AccessControl.PropagationFlags')
+    $type = 'Allow'                                   # Other options: [enum]::GetValues('System.Securit y.AccessControl.AccessControlType')
+    $ACE = New-Object System.Security.AccessControl.FileSystemAccessRule($identity, $rights, $inheritance, $propagation, $type)
+
+    # Get-ChildItem $dir -Directory -Recurse | Select-Object -ExcludeProperty FullName | ForEach-Object {
+    Get-ChildItem $dir -Directory -Recurse | ForEach-Object {
+            $ACL = Get-Acl -Path $_.FullName
+            $ACL.AddAccessRule($ACE)
+            Set-Acl -Path $_.FullName -AclObject $ACL
+            Write-Host "$_.FullName is done."
+        }
+}
