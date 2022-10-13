8a7193c45a27deb5f733bed21d8d65df54260d43
brucelin michael <michaelbrucelin@163.com>
2022-10-13 09:45:34 +0800

    20221013-0945

---
+++ Algorithm/Book/大话数据结构 溢彩加强版/my_implement_csharp/my_implement_csharp/第5章 串/02模式匹配_KMP.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace my_implement_csharp.第5章_串
+{
+    public class _02模式匹配_KMP
+    {
+        public int[] GetNext(string s)
+        {
+            int[] result = new int[s.Length];
+
+            return result;
+        }
+    }
+}
