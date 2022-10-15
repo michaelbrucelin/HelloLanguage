8bde31c44af17dadd01362dc4ca60c75cde7e214
brucelin michael <michaelbrucelin@163.com>
2022-10-15 16:33:11 +0800

    20221015-1633

---
+++ Language/C#/PlayGround/LeetCode/LeetCode/QuestionBank/Question0021/Interface0021.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question0021
+{
+    public interface Interface0021
+    {
+        public ListNode MergeTwoLists(ListNode list1, ListNode list2);
+    }
+}
---
+++ Language/C#/PlayGround/LeetCode/LeetCode/QuestionBank/Question0021/Solution0021_2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question0021
+{
+    public class Solution0021_2 : Interface0021
+    {
+        /// <summary>
+        /// 递归
+        /// </summary>
+        /// <param name="list1"></param>
+        /// <param name="list2"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        {
+            if (list1 == null) return list2;
+            if (list2 == null) return list1;
+
+            if (list1.val <= list2.val)
+            {
+                list1.next = MergeTwoLists(list1.next, list2);
+                return list1;
+            }
+            else
+            {
+                list2.next = MergeTwoLists(list1, list2.next);
+                return list2;
+            }
+        }
+    }
+}
