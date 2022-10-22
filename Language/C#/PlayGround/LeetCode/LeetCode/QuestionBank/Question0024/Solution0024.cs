9c4aeaaaf736c58024d671e83a93d58f9477e540
brucelin michael <michaelbrucelin@163.com>
2022-10-22 17:15:08 +0800

    20221022-1715

---
+++ Language/C#/PlayGround/LeetCode/LeetCode/QuestionBank/Question0024/Solution0024.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question0024
+{
+    public class Solution0024 : Interface0024
+    {
+        public ListNode SwapPairs(ListNode head)
+        {
+            if (head == null || head.next == null) return head;
+
+            ListNode header = new ListNode(); header.next = head;
+            ListNode ptrPerv = header;
+            ListNode ptrA = ptrPerv.next;
+            ListNode ptrB = ptrA.next;
+            while (true)
+            {
+                ptrPerv.next = ptrB; ptrA.next = ptrB.next; ptrB.next = ptrA;  // 交换
+
+                if (ptrA.next != null && ptrA.next.next != null)
+                {
+                    ptrPerv = ptrA; ptrA = ptrPerv.next; ptrB = ptrA.next;     // 移动指针
+                }
+                else break;
+            }
+
+            return header.next;
+        }
+    }
+}
---
+++ Language/C#/PlayGround/LeetCode/LeetCode/QuestionBank/Question0024/Solution0024_2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question0024
+{
+    public class Solution0024_2 : Interface0024
+    {
+        public ListNode SwapPairs(ListNode head)
+        {
+            if (head == null || head.next == null) return head;
+
+            ListNode head_new = head.next;
+            head.next = SwapPairs(head_new.next);
+            head_new.next = head;
+
+            return head_new;
+        }
+    }
+}
