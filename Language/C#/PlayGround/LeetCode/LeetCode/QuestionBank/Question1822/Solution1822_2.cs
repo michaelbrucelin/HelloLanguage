1781ce49d116752dceea61706cc834e6bac335c0
brucelin michael <michaelbrucelin@163.com>
2022-10-27 09:14:43 +0800

    20221027-0914

--- Language/C#/PlayGround/LeetCode/LeetCode/QuestionBank/Question1822/Solution1822_2.cs
+++ Language/C#/PlayGround/LeetCode/LeetCode/QuestionBank/Question1822/Solution1822_2.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace LeetCode.QuestionBank.Question1822
 {
-    internal class Solution1822_2
+    public class Solution1822_2 : Interface1822
     {
+        public int ArraySign(int[] nums)
+        {
+            if (nums.Contains(0)) return 0;
+            if ((nums.Count(i => i < 0) & 1) == 1) return -1;
+            return 1;
+        }
     }
 }
