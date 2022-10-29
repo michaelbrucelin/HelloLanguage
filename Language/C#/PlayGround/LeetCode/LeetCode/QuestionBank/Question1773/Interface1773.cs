4e0fac0ebd12cbfe0c044b09972b1d106ae3dd2a
brucelin michael <michaelbrucelin@163.com>
2022-10-29 09:12:57 +0800

    20221029-0912

---
+++ Language/C#/PlayGround/LeetCode/LeetCode/QuestionBank/Question1773/Interface1773.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1773
+{
+    public interface Interface1773
+    {
+        public int CountMatches(IList<IList<string>> items, string ruleKey, string ruleValue);
+    }
+}
