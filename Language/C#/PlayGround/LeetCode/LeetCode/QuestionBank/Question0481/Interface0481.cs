953d077c03eb23f46c3c5efbbfa12e0bcfaa8fc2
brucelin michael <michaelbrucelin@163.com>
2022-10-31 14:14:20 +0800

    20221031-1414

---
+++ Language/C#/PlayGround/LeetCode/LeetCode/QuestionBank/Question0481/Interface0481.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question0481
+{
+    public interface Interface0481
+    {
+        public int MagicalString(int n);
+    }
+}
---
+++ Language/C#/PlayGround/LeetCode/LeetCode/QuestionBank/Question0481/Test0481.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question0481
+{
+    public class Test0481
+    {
+        public void Test()
+        {
+            Interface0481 solution = new Solution0481();
+            int n;
+            int result, answer;
+            int id = 0;
+
+            // 1.
+            n = 6; answer = 3;
+            result = solution.MagicalString(n);
+            Console.WriteLine($"{++id,2}: {result == answer}, result: {result}, answer: {answer}");
+
+            // 2.
+            n = 1; answer = 1;
+            result = solution.MagicalString(n);
+            Console.WriteLine($"{++id,2}: {result == answer}, result: {result}, answer: {answer}");
+
+            // 3.
+            n = 100; answer = 49;
+            result = solution.MagicalString(n);
+            Console.WriteLine($"{++id,2}: {result == answer}, result: {result}, answer: {answer}");
+
+            // 4.
+            n = 9999; answer = 4995;
+            result = solution.MagicalString(n);
+            Console.WriteLine($"{++id,2}: {result == answer}, result: {result}, answer: {answer}");
+
+            // 5.
+            n = 100000; answer = 49972;
+            result = solution.MagicalString(n);
+            Console.WriteLine($"{++id,2}: {result == answer}, result: {result}, answer: {answer}");
+        }
+    }
+}
