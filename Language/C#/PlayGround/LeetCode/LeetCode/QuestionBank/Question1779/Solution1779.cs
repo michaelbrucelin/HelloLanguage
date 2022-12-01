2dafbe6dcc121f8a2246a104f21564375296e13b
brucelin michael <michaelbrucelin@163.com>
2022-12-01 11:36:25 +0800

    20221201-1136

---
+++ Language/C#/PlayGround/LeetCode/LeetCode/QuestionBank/Question1779/Solution1779.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.QuestionBank.Question1779
+{
+    public class Solution1779 : Interface1779
+    {
+        public int NearestValidPoint(int x, int y, int[][] points)
+        {
+            int result = -1, distance = int.MaxValue;
+            for (int i = 0; i < points.Length; i++)
+            {
+                if (points[i][0] == x || points[i][1] == y)
+                {
+                    int _distance = Math.Abs(points[i][0] - x) + Math.Abs(points[i][1] - y);
+                    if (_distance < distance)
+                    {
+                        result = i;
+                        distance = _distance;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public int NearestValidPoint2(int x, int y, int[][] points)
+        {
+            int result = -1, distance = int.MaxValue;
+            for (int i = 0; i < points.Length; i++)
+            {
+                if (points[i][0] == x)
+                {
+                    int _distance = Math.Abs(points[i][1] - y);
+                    if (_distance < distance)
+                    {
+                        result = i;
+                        distance = _distance;
+                    }
+                }
+                else if (points[i][1] == y)
+                {
+                    int _distance = Math.Abs(points[i][0] - x);
+                    if (_distance < distance)
+                    {
+                        result = i;
+                        distance = _distance;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
