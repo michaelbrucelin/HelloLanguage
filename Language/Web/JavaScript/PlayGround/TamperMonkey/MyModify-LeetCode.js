548236d9889802ab70f50778a609138f74d4f06b
brucelin michael <michaelbrucelin@163.com>
2023-03-29 14:52:44 +0800

    20230329-145244

--- Language/Web/JavaScript/PlayGround/TamperMonkey/MyModify-LeetCode.js
+++ Language/Web/JavaScript/PlayGround/TamperMonkey/MyModify-LeetCode.js
@@ -42,11 +42,17 @@
         //}
     });
 
-    // 关闭跳转到中文版的提示
-    // item = document.querySelector("#__next > div.\\!hidden.md\\:\\!flex.css-1vvfvd7");
-    // if(item) item.remove();
-    var btn = document.querySelector("#cn-banner > div > div > div.cn-close-btn");
-    if(btn) btn.click();
+    // 关闭跳转到中文版的提示（不好用，没继续优化）
+    let page = document.querySelector("#__next");
+    page.addEventListener("DOMNodeInserted", function (event) {
+        var btn_cn, times = 10;
+        while (!btn_cn && times-- > 0) {
+            // item = document.querySelector("#__next > div.\\!hidden.md\\:\\!flex.css-1vvfvd7");
+            // if(item) item.remove();
+            btn_cn = document.querySelector("#cn-banner > div > div > div.cn-close-btn");
+            if(btn_cn) btn_cn.click();
+        }
+    });
 
     /* 采用这种方式设置没有生效，但是同样的方式在“有道云翻译”页面是生效的，究竟是为什么没弄明白，使用下面自定义的GM_addStyle(css)是好用的
         // @grant        GM_addStyle
