f972d69a18ee68f6c27ee9092d284a97f3dcbb1c
mlin <36387756+michaelbrucelin@users.noreply.github.com>
2023-11-07 09:32:16 +0800

    20231107-093215

--- Language/C#/Template/MyUtils/Utilses/MyUtilsMath.cs
+++ Language/C#/Template/MyUtils/Utilses/MyUtilsMath.cs
@@ -158,5 +158,26 @@ namespace TestCSharp
             return x << move;
         }
         #endregion
+
+        #region 判断是否为质数
+        /// <summary>
+        /// 判断是否为质数
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public static bool IsPrime(int number)
+        {
+            if (number <= 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+
+            int boundary = (int)Math.Floor(Math.Sqrt(number));
+
+            for (int i = 3; i <= boundary; i += 2)
+                if (number % i == 0) return false;
+
+            return true;
+        }
+        #endregion
     }
 }
