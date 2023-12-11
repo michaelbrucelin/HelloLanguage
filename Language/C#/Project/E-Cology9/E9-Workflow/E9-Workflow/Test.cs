a87690f3cf46d1ddd521d67e03330f7ad5483dae
mlin <36387756+michaelbrucelin@users.noreply.github.com>
2023-12-11 13:54:45 +0800

    20231211-135444

--- Language/C#/Project/E-Cology9/E9-Workflow/E9-Workflow/Test.cs
+++ Language/C#/Project/E-Cology9/E9-Workflow/E9-Workflow/Test.cs
@@ -8,7 +8,7 @@ using WF = E9_Workflow.E9WorkflowService;
 
 namespace E9_Workflow
 {
-#if false
+    // #if false
     public class Test
     {
         private readonly Random random = new Random();
@@ -101,22 +101,24 @@ namespace E9_Workflow
                 new WF.WorkflowRequestTableField(){ fieldName = "content", fieldValue="道可道 非常道 名可名 非常名<br>无 名天地之始 有 名万物之母", view = true, edit = true }
             };
 
-            //WF.WorkflowRequestTableRecord r2 = new WF.WorkflowRequestTableRecord();
-            //r2.workflowRequestTableFields = new WF.WorkflowRequestTableField[] {
-            //    new WF.WorkflowRequestTableField(){ fieldName = "chrm", fieldValue = "133", view = true, edit = true },
-            //    new WF.WorkflowRequestTableField(){ fieldName = "cdept", fieldValue = "22", view = true, edit = true },
-            //    new WF.WorkflowRequestTableField(){ fieldName = "cdate", fieldValue = DateTime.Now.ToString("yyyy-MM-dd"), view = true, edit = true },
-            //    new WF.WorkflowRequestTableField(){ fieldName = "toTell", fieldValue = "58,26,133,148", view = true, edit = true },
-            //    new WF.WorkflowRequestTableField(){ fieldName = "content", fieldValue="天行健 君子当自强不息<br>地势坤 君子当厚德载物", view = true, edit = true }
-            //};
+            /*
+            WF.WorkflowRequestTableRecord r2 = new WF.WorkflowRequestTableRecord();
+            r2.workflowRequestTableFields = new WF.WorkflowRequestTableField[] {
+                new WF.WorkflowRequestTableField(){ fieldName = "chrm", fieldValue = "133", view = true, edit = true },
+                new WF.WorkflowRequestTableField(){ fieldName = "cdept", fieldValue = "22", view = true, edit = true },
+                new WF.WorkflowRequestTableField(){ fieldName = "cdate", fieldValue = DateTime.Now.ToString("yyyy-MM-dd"), view = true, edit = true },
+                new WF.WorkflowRequestTableField(){ fieldName = "toTell", fieldValue = "58,26,133,148", view = true, edit = true },
+                new WF.WorkflowRequestTableField(){ fieldName = "content", fieldValue="天行健 君子当自强不息<br>地势坤 君子当厚德载物", view = true, edit = true }
+            };
+            */
 
             WF.WorkflowMainTableInfo mainTable = new WF.WorkflowMainTableInfo();    // 主表单信息
             mainTable.requestRecords = new WF.WorkflowRequestTableRecord[] { r1 };  // 尽管主表单只有一份信息，但是这个位置仍然被设计成数组，不确认为什么要这么设计
                                                                                     // 尝试过给2份主表单信息，仍然只有第一份信息（数组第一项）有效
                                                                                     //mainTable.requestRecords = new WF.WorkflowRequestTableRecord[] { r1, r2 };
 
-            //E9WF.WorkflowDetailTableInfo detailTable = new E9WF.WorkflowDetailTableInfo();
-            //detailTable.workflowRequestTableRecords = new E9WF.WorkflowRequestTableRecord[10];
+            // E9WF.WorkflowDetailTableInfo detailTable = new E9WF.WorkflowDetailTableInfo();
+            // detailTable.workflowRequestTableRecords = new E9WF.WorkflowRequestTableRecord[10];
 
             requestInfo.requestName = "hello webservices " + DateTime.Now.ToString("yyyy-MM-dd");  // 流程标题
             requestInfo.requestLevel = "2";                                                        // 紧急程度：0正常 1重要 2紧急
@@ -196,5 +198,5 @@ namespace E9_Workflow
             return result;
         }
     }
-#endif
+    // #endif
 }
--- Language/C#/Project/E-Cology9/E9-Workflow/HelloWorkflow/Form1.cs
+++ Language/C#/Project/E-Cology9/E9-Workflow/HelloWorkflow/Form1.cs
@@ -27,8 +27,8 @@ namespace HelloWorkflow
         {
             Test test = new Test();
 
-            //string result = test.TestWithDetail();
-            //MessageBox.Show(result);
+            // string result = test.TestWithDetail();
+            // MessageBox.Show(result);
 
             string result = test.Test0();
             MessageBox.Show(result);
@@ -39,170 +39,175 @@ namespace HelloWorkflow
             Workflow wf = new Workflow();
 
             #region 使用json创建流程
-            //string data1 = @"{
-            //  'creater': 58,
-            //  'requestName': '费用报销申请—张三—1900-01-01',
-            //  'requestLevel': '1',
+            /*
+            string data1 = @"{
+              'creater': 58,
+              'requestName': '费用报销申请—张三—1900-01-01',
+              'requestLevel': '1',
 
-            //  'baseInfo': {
-            //    'workflowId': '4'
-            //  },
+              'baseInfo': {
+                'workflowId': '4'
+              },
 
-            //  'mainTableInfo': {
-            //    'requestRecords': [
-            //      {
-            //        'workflowRequestTableFields': [
-            //          { 'fieldName': 'chrm', 'fieldValue': '133', 'view': true, 'edit': true },
-            //          { 'fieldName': 'cdept', 'fieldValue': '22', 'view': true, 'edit': true },
-            //          { 'fieldName': 'cdate', 'fieldValue': '2021-01-01', 'view': true, 'edit': true },
-            //          { 'fieldName': 'managerCnt', 'fieldValue': '2', 'view': true, 'edit': true },
-            //          { 'fieldName': 'reason', 'fieldValue': 'hello world', 'view': true, 'edit': true },
-            //          { 'fieldName': 'bxfs', 'fieldValue': '1', 'view': true, 'edit': true }
-            //        ]
-            //      }
-            //    ]
-            //  },
+              'mainTableInfo': {
+                'requestRecords': [
+                  {
+                    'workflowRequestTableFields': [
+                      { 'fieldName': 'chrm', 'fieldValue': '133', 'view': true, 'edit': true },
+                      { 'fieldName': 'cdept', 'fieldValue': '22', 'view': true, 'edit': true },
+                      { 'fieldName': 'cdate', 'fieldValue': '2021-01-01', 'view': true, 'edit': true },
+                      { 'fieldName': 'managerCnt', 'fieldValue': '2', 'view': true, 'edit': true },
+                      { 'fieldName': 'reason', 'fieldValue': 'hello world', 'view': true, 'edit': true },
+                      { 'fieldName': 'bxfs', 'fieldValue': '1', 'view': true, 'edit': true }
+                    ]
+                  }
+                ]
+              },
 
-            //  'detailTableInfos': [
-            //    {
-            //      'workflowRequestTableRecords': [
-            //        {
-            //          'workflowRequestTableFields': [
-            //            { 'fieldName': 'bxlx', 'fieldValue': '0', 'view': true, 'edit': true },
-            //            { 'fieldName': 'fjs', 'fieldValue': '0', 'view': true, 'edit': true },
-            //            { 'fieldName': 'shenbaobz', 'fieldValue': '0', 'view': true, 'edit': true },
-            //            { 'fieldName': 'shenbaoje', 'fieldValue': '100', 'view': true, 'edit': true }
-            //          ]
-            //        },
-            //        {
-            //          'workflowRequestTableFields': [
-            //            { 'fieldName': 'bxlx', 'fieldValue': '1', 'view': true, 'edit': true },
-            //            { 'fieldName': 'fjs', 'fieldValue': '0', 'view': true, 'edit': true },
-            //            { 'fieldName': 'shenbaobz', 'fieldValue': '1', 'view': true, 'edit': true },
-            //            { 'fieldName': 'shenbaoje', 'fieldValue': '200', 'view': true, 'edit': true }
-            //          ]
-            //        }
-            //      ]
-            //    }
-            //  ]
-            //}";
-            //string data2 = @"{
-            //  'creater': 58,
-            //  'requestName': '内部留言—张三—1900-01-01',
-            //  'requestLevel': '1',
+              'detailTableInfos': [
+                {
+                  'workflowRequestTableRecords': [
+                    {
+                      'workflowRequestTableFields': [
+                        { 'fieldName': 'bxlx', 'fieldValue': '0', 'view': true, 'edit': true },
+                        { 'fieldName': 'fjs', 'fieldValue': '0', 'view': true, 'edit': true },
+                        { 'fieldName': 'shenbaobz', 'fieldValue': '0', 'view': true, 'edit': true },
+                        { 'fieldName': 'shenbaoje', 'fieldValue': '100', 'view': true, 'edit': true }
+                      ]
+                    },
+                    {
+                      'workflowRequestTableFields': [
+                        { 'fieldName': 'bxlx', 'fieldValue': '1', 'view': true, 'edit': true },
+                        { 'fieldName': 'fjs', 'fieldValue': '0', 'view': true, 'edit': true },
+                        { 'fieldName': 'shenbaobz', 'fieldValue': '1', 'view': true, 'edit': true },
+                        { 'fieldName': 'shenbaoje', 'fieldValue': '200', 'view': true, 'edit': true }
+                      ]
+                    }
+                  ]
+                }
+              ]
+            }";
+            string data2 = @"{
+              'creater': 58,
+              'requestName': '内部留言—张三—1900-01-01',
+              'requestLevel': '1',
 
-            //  'baseInfo': {
-            //    'workflowId': '2'
-            //  },
+              'baseInfo': {
+                'workflowId': '2'
+              },
 
-            //  'mainTableInfo': {
-            //    'requestRecords': [
-            //      {
-            //        'workflowRequestTableFields': [
-            //          { 'fieldName': 'chrm', 'fieldValue': '133', 'view': true, 'edit': true },
-            //          { 'fieldName': 'cdept', 'fieldValue': '22', 'view': true, 'edit': true },
-            //          { 'fieldName': 'cdate', 'fieldValue': '2021-01-01', 'view': true, 'edit': true },
-            //          { 'fieldName': 'toTell', 'fieldValue': '58,26,133,148', 'view': true, 'edit': true },
-            //          { 'fieldName': 'content', 'fieldValue': '君不见 黄河之水天上来 奔流到海不复回<br>君不见 高堂明镜悲白发 朝如青丝暮成雪', 'view': true, 'edit': true }
-            //        ]
-            //      }
-            //    ]
-            //  }
-            //}";
+              'mainTableInfo': {
+                'requestRecords': [
+                  {
+                    'workflowRequestTableFields': [
+                      { 'fieldName': 'chrm', 'fieldValue': '133', 'view': true, 'edit': true },
+                      { 'fieldName': 'cdept', 'fieldValue': '22', 'view': true, 'edit': true },
+                      { 'fieldName': 'cdate', 'fieldValue': '2021-01-01', 'view': true, 'edit': true },
+                      { 'fieldName': 'toTell', 'fieldValue': '58,26,133,148', 'view': true, 'edit': true },
+                      { 'fieldName': 'content', 'fieldValue': '君不见 黄河之水天上来 奔流到海不复回<br>君不见 高堂明镜悲白发 朝如青丝暮成雪', 'view': true, 'edit': true }
+                    ]
+                  }
+                ]
+              }
+            }";
 
-            //var result = wf.CreateWF(data2);
-            //MessageBox.Show(result.desc);
+            var result = wf.CreateWF(data2);
+            MessageBox.Show(result.desc);
+            */
             #endregion
 
             #region 使用对象创建流程
-            //Dictionary<string, string> mainTable = new Dictionary<string, string>();
-            //mainTable.Add("chrm", "133");
-            //mainTable.Add("cdept", "22");
-            //mainTable.Add("cdate", "2020-01-01");
-            //mainTable.Add("toTell", "58,26");
-            //mainTable.Add("content", "hello world");
+            /*
+            Dictionary<string, string> mainTable = new Dictionary<string, string>();
+            mainTable.Add("chrm", "133");
+            mainTable.Add("cdept", "22");
+            mainTable.Add("cdate", "2020-01-01");
+            mainTable.Add("toTell", "58,26");
+            mainTable.Add("content", "hello world");
 
-            //Dictionary<string, string> mainTable = new Dictionary<string, string>();
-            //mainTable.Add("chrm", "133");
-            //mainTable.Add("cdept", "22");
-            //mainTable.Add("cdate", "2020-01-01");
-            //mainTable.Add("managerCnt", "2");
-            //mainTable.Add("reason", "hello world");
-            //mainTable.Add("costType", "1");
-            //mainTable.Add("bxfs", "1");
-            //mainTable.Add("beizhu", "hello world");
+            Dictionary<string, string> mainTable = new Dictionary<string, string>();
+            mainTable.Add("chrm", "133");
+            mainTable.Add("cdept", "22");
+            mainTable.Add("cdate", "2020-01-01");
+            mainTable.Add("managerCnt", "2");
+            mainTable.Add("reason", "hello world");
+            mainTable.Add("costType", "1");
+            mainTable.Add("bxfs", "1");
+            mainTable.Add("beizhu", "hello world");
 
-            //List<Dictionary<string, string>>[] detailTables = new List<Dictionary<string, string>>[1];
-            //Dictionary<string, string> r1 = new Dictionary<string, string>();
-            //r1.Add("bxlx", "0");
-            //r1.Add("fjs", "1");
-            //r1.Add("shenbaobz", "0");
-            //r1.Add("shenbaoje", "123");
-            //Dictionary<string, string> r2 = new Dictionary<string, string>();
-            //r2.Add("bxlx", "1");
-            //r2.Add("fjs", "2");
-            //r2.Add("shenbaobz", "1");
-            //r2.Add("shenbaoje", "100");
-            //Dictionary<string, string> r3 = new Dictionary<string, string>();
-            //r3.Add("bxlx", "0");
-            //r3.Add("fjs", "3");
-            //r3.Add("shenbaobz", "0");
-            //r3.Add("shenbaoje", "64");
-            //detailTables[0] = new List<Dictionary<string, string>>() { r1, r2, r3 };
+            List<Dictionary<string, string>>[] detailTables = new List<Dictionary<string, string>>[1];
+            Dictionary<string, string> r1 = new Dictionary<string, string>();
+            r1.Add("bxlx", "0");
+            r1.Add("fjs", "1");
+            r1.Add("shenbaobz", "0");
+            r1.Add("shenbaoje", "123");
+            Dictionary<string, string> r2 = new Dictionary<string, string>();
+            r2.Add("bxlx", "1");
+            r2.Add("fjs", "2");
+            r2.Add("shenbaobz", "1");
+            r2.Add("shenbaoje", "100");
+            Dictionary<string, string> r3 = new Dictionary<string, string>();
+            r3.Add("bxlx", "0");
+            r3.Add("fjs", "3");
+            r3.Add("shenbaobz", "0");
+            r3.Add("shenbaoje", "64");
+            detailTables[0] = new List<Dictionary<string, string>>() { r1, r2, r3 };
 
-            //var result = wf.CreateWF("4", "create by object", "2", 58, mainTable, detailTables);
-            //MessageBox.Show(result.desc);
+            var result = wf.CreateWF("4", "create by object", "2", 58, mainTable, detailTables);
+            MessageBox.Show(result.desc);
+            */
             #endregion
 
             #region 使用json生成器创建流程
-            //Dictionary<string, string> mainTable = new Dictionary<string, string>();
-            //mainTable.Add("chrm", "133");
-            //mainTable.Add("cdept", "22");
-            //mainTable.Add("cdate", "2020-01-01");
-            //mainTable.Add("toTell", "58,26");
-            //mainTable.Add("content", "hello world");
+            /*
+            Dictionary<string, string> mainTable = new Dictionary<string, string>();
+            mainTable.Add("chrm", "133");
+            mainTable.Add("cdept", "22");
+            mainTable.Add("cdate", "2020-01-01");
+            mainTable.Add("toTell", "58,26");
+            mainTable.Add("content", "hello world");
 
-            //Dictionary<string, string> mainTable = new Dictionary<string, string>();
-            //mainTable.Add("chrm", "133");
-            //mainTable.Add("cdept", "22");
-            //mainTable.Add("cdate", "2020-01-01");
-            //mainTable.Add("managerCnt", "2");
-            //mainTable.Add("reason", "hello world");
-            //mainTable.Add("costType", "1");
-            //mainTable.Add("bxfs", "1");
-            //mainTable.Add("beizhu", "hello world");
+            Dictionary<string, string> mainTable = new Dictionary<string, string>();
+            mainTable.Add("chrm", "133");
+            mainTable.Add("cdept", "22");
+            mainTable.Add("cdate", "2020-01-01");
+            mainTable.Add("managerCnt", "2");
+            mainTable.Add("reason", "hello world");
+            mainTable.Add("costType", "1");
+            mainTable.Add("bxfs", "1");
+            mainTable.Add("beizhu", "hello world");
 
-            //List<Dictionary<string, string>>[] detailTables = new List<Dictionary<string, string>>[1];
-            //Dictionary<string, string> r1 = new Dictionary<string, string>();
-            //r1.Add("bxlx", "0");
-            //r1.Add("fjs", "1");
-            //r1.Add("shenbaobz", "0");
-            //r1.Add("shenbaoje", "123");
-            //Dictionary<string, string> r2 = new Dictionary<string, string>();
-            //r2.Add("bxlx", "1");
-            //r2.Add("fjs", "2");
-            //r2.Add("shenbaobz", "1");
-            //r2.Add("shenbaoje", "100");
-            //Dictionary<string, string> r3 = new Dictionary<string, string>();
-            //r3.Add("bxlx", "0");
-            //r3.Add("fjs", "3");
-            //r3.Add("shenbaobz", "0");
-            //r3.Add("shenbaoje", "64");
-            //detailTables[0] = new List<Dictionary<string, string>>() { r1, r2, r3 };
+            List<Dictionary<string, string>>[] detailTables = new List<Dictionary<string, string>>[1];
+            Dictionary<string, string> r1 = new Dictionary<string, string>();
+            r1.Add("bxlx", "0");
+            r1.Add("fjs", "1");
+            r1.Add("shenbaobz", "0");
+            r1.Add("shenbaoje", "123");
+            Dictionary<string, string> r2 = new Dictionary<string, string>();
+            r2.Add("bxlx", "1");
+            r2.Add("fjs", "2");
+            r2.Add("shenbaobz", "1");
+            r2.Add("shenbaoje", "100");
+            Dictionary<string, string> r3 = new Dictionary<string, string>();
+            r3.Add("bxlx", "0");
+            r3.Add("fjs", "3");
+            r3.Add("shenbaobz", "0");
+            r3.Add("shenbaoje", "64");
+            detailTables[0] = new List<Dictionary<string, string>>() { r1, r2, r3 };
 
-            //QRequestInfoBuilder rb = new QRequestInfoBuilder();
-            //rb.creater = 58;
-            //rb.requestName = "create by json generator";
-            //rb.requestLevel = "2";
-            //rb.workflowid = "2";
-            //rb.mainTable = mainTable;
-            //rb.detailTables = detailTables;
+            QRequestInfoBuilder rb = new QRequestInfoBuilder();
+            rb.creater = 58;
+            rb.requestName = "create by json generator";
+            rb.requestLevel = "2";
+            rb.workflowid = "2";
+            rb.mainTable = mainTable;
+            rb.detailTables = detailTables;
 
+            MessageBox.Show(rb.Generator());
 
-            //MessageBox.Show(rb.Generator());
-
-            //var result = wf.CreateWF(rb.Generator());
-            //MessageBox.Show(result.desc);
+            var result = wf.CreateWF(rb.Generator());
+            MessageBox.Show(result.desc);
+            */
             #endregion
         }
 
