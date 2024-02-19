820ace811d5a06dbbd7cdec4d9afa2e2d222301e
mlin <36387756+michaelbrucelin@users.noreply.github.com>
2024-02-19 14:16:12 +0800

    20240219-141611

--- Language/Batch/PowerShell/Project/MyMove-IMWindows.ps1
+++ Language/Batch/PowerShell/Project/MyMove-IMWindows.ps1
@@ -9,7 +9,8 @@ function MyMove-IMWindows() {
         Set-ExecutionPolicy $execplcy -Force
     }
 
-    if ($rtx_p = Get-Process -Name rtx -ErrorAction SilentlyContinue) {
+    # RTX
+    if ($rtx_p = Get-Process -Name 'rtx' -ErrorAction SilentlyContinue) {
         # 主窗体
         $rtx_w = Select-UIElement -PID $rtx_p.Id -ControlType Pane
         $rtx_w | Invoke-Transform.Move -x 1666 -y 5 -ErrorAction SilentlyContinue
@@ -20,13 +21,15 @@ function MyMove-IMWindows() {
         $rtx_w | Invoke-Transform.Resize -width 783 -height 783 -ErrorAction SilentlyContinue
     }
 
-    if ($em_p = Get-Process -Name E-Mobile -ErrorAction SilentlyContinue) {
-        $em_w = Select-UIElement -PID $em_p.Id -WindowName E-Mobile -ControlType Window
+    # E-Mobile
+    if ($em_p = Get-Process -Name 'E-Mobile' -ErrorAction SilentlyContinue) {
+        $em_w = Select-UIElement -PID $em_p.Id -WindowName 'E-Mobile' -ControlType Window
         $em_w | Invoke-Transform.Move -x 923 -y 106 -ErrorAction SilentlyContinue
         $em_w | Invoke-Transform.Resize -width 1000 -height 788 -ErrorAction SilentlyContinue
     }
 
-    if ($qq_p = Get-Process -Name QQ -ErrorAction SilentlyContinue) {
+    # QQ
+    if ($qq_p = Get-Process -Name 'QQ' -ErrorAction SilentlyContinue) {
         # 主窗体
         $qq_w = Select-UIElement -PID $qq_p.Id -ControlType Pane | Where-Object { $_.Name -eq 'QQ' }
         $qq_w | Invoke-Transform.Move -x 1624 -y -3 -ErrorAction SilentlyContinue
@@ -35,5 +38,19 @@ function MyMove-IMWindows() {
         $qq_w = Select-UIElement -PID $qq_p.Id -ControlType Pane | Where-Object { $_.Name -ne 'QQ' }
         $qq_w | Invoke-Transform.Move -x 1138 -y 103 -ErrorAction SilentlyContinue
         $qq_w | Invoke-Transform.Resize -width 826 -height 793 -ErrorAction SilentlyContinue
+    }
+
+    # WeChat
+    if ($wc_p = Get-Process -Name 'WeChat' -ErrorAction SilentlyContinue) {
+        $wc_w = Select-UIElement -PID $wc_p.Id -WindowName '微信' -ControlType Window
+        $wc_w | Invoke-Transform.Move -x 923 -y 106 -ErrorAction SilentlyContinue
+        $wc_w | Invoke-Transform.Resize -width 1000 -height 788 -ErrorAction SilentlyContinue
+    }
+
+    # WXWork
+    if ($wc_p = Get-Process -Name 'WXWork' -ErrorAction SilentlyContinue) {
+        $wc_w = Select-UIElement -PID $wc_p.Id -WindowName '企业微信' -ControlType Window
+        $wc_w | Invoke-Transform.Move -x 923 -y 106 -ErrorAction SilentlyContinue
+        $wc_w | Invoke-Transform.Resize -width 1000 -height 788 -ErrorAction SilentlyContinue
     }
 }
