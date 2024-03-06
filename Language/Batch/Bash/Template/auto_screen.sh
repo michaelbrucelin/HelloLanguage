bcad9ccd15e0f1ea9a0cf71a1a077b65e38fa0bf
mlin <36387756+michaelbrucelin@users.noreply.github.com>
2024-03-06 15:01:03 +0800

    20240306-150103

--- Language/Batch/Bash/Template/auto_screen.sh
+++ Language/Batch/Bash/Template/auto_screen.sh
@@ -19,32 +19,36 @@ parameters=$(getopt -o I:i:c: -l name:,list::,remote: -n "$0" -- "$@")
 [ $? != 0 ] && usage 
 
 or_op=$(echo "$parameters" | grep -Eo '\-\-name|\-\-list' | wc -l)
-[ "$or_op" -gt 1 ] && { echo "--name and --list cannot be together"; usage; }
+[ "$or_op" -gt 1 ] && { echo "--name and --list cannot be exist together"; usage; }
 or_op=$(echo "$parameters" | grep -Eo '\-\-name|\-\-remote' | wc -l)
-[ "$or_op" -gt 1 ] && { echo "--name and --remote cannot be together"; usage; }
+[ "$or_op" -gt 1 ] && { echo "--name and --remote cannot be exist together"; usage; }
 or_op=$(echo "$parameters" | grep -Eo '\-\-list|\-\-remote' | wc -l)
-[ "$or_op" -gt 1 ] && { echo "--list and --remote cannot be together"; usage; }
+[ "$or_op" -gt 1 ] && { echo "--list and --remote cannot be exist together"; usage; }
 
 eval set -- "${parameters}"
 while true ; do
     case "$1" in
         --list)
             case "$2" in
-                "") list=''; shift 2 ;;
-                *)  list=$2; shift 2 ;;
+                "") list=' '; shift 2 ;;
+                *)  list=$2;  shift 2 ;;
             esac ;;
-        --remote) remote=$2; shift 2 ;;
-        --name)   name=$2;   shift 2 ;;
-        -I)       I=$2;      shift 2 ;;
-        -i)       i=$2;      shift 2 ;;
-        -c)       c=$2;      shift 2 ;;
+        --remote) remote=$2;  shift 2 ;;
+        --name)   name=$2;    shift 2 ;;
+        -I)       I=$2;       shift 2 ;;
+        -i)       i=$2;       shift 2 ;;
+        -c)       c=$2;       shift 2 ;;
         --) shift; break ;;
         *) usage ;;
     esac
 done
 
 if [ -n "${list}" ]; then
-    screen -list "${list}"
+    if [ "${list}" == " " ]; then
+        screen -list | grep -v '/var/run/screen/'
+    else
+        screen -list | grep -v '/var/run/screen/' | grep "${list}"
+    fi
     exit 0
 fi
 
@@ -57,7 +61,20 @@ if [ -z "${name}" ] || [ -z "${I}" ] || [ -z "${i}" ] || [ -z "${c}" ] || [ $# -
     usage
 fi
 
-if ! screen -list | grep -q "myscreen"; then
-    echo "new"
-    # run bash script
+if ! screen -list | grep -q "${name}"; then
+    screen -S "${name}" bash -c "ping $1 -I ${I} -i ${i} -c ${c}"
+else
+    echo "a remote window with the same name already exists"
+    exit 1
 fi
+
+# if [ -n "${list}" ]; then echo "\$list is: ${list}"; else echo "\$list is not exists"; fi
+# if [ -n "${remote}" ]; then echo "\$remote is: ${remote}"; else echo "\$remote is not exists"; fi
+# if [ -n "${name}" ]; then echo "\$name is: ${name}"; else echo "\$name is not exists"; fi
+# if [ -n "${I}" ]; then echo "\$I is: ${I}"; else echo "\$I is not exists"; fi
+# if [ -n "${i}" ]; then echo "\$i is: ${i}"; else echo "\$i is not exists"; fi
+# if [ -n "${c}" ]; then echo "\$c is: ${c}"; else echo "\$c is not exists"; fi
+# if [ -n "${1}" ]; then echo "\$1 is: ${1}"; else echo "\$1 is not exists"; fi
+# if [ -n "${2}" ]; then echo "\$2 is: ${2}"; else echo "\$2 is not exists"; fi
+# if [ -n "${3}" ]; then echo "\$3 is: ${3}"; else echo "\$3 is not exists"; fi
+# echo "the count of parameters is $#"
